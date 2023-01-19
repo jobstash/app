88cf35002eb0346f1a5fa8e2094071e09f9ee4ee
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2023-01-19 17:59:44 +0100

    fix path templates/views

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
- content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
- theme: {
-  extend: {},
- },
- plugins: [],
-};
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/stories/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
