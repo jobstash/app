b4d3ec1a49a84ae8e27af7327429caf69296a729
John Ballesteros <johnshift.dev@gmail.com>
2023-01-24 15:07:19 +0800

    chore: setup apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import '~/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+import { ApolloProvider } from '@apollo/client';
+
+import { client } from '~/graphql/apollo-client';
+
 if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
   const { worker } = require('~/mocks/browser');
   worker.start();
 }
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
 );
 
 export default App;
