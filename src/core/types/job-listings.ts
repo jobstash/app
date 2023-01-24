088c779086114d0ba8cce6e832f9e8ed24117083
John Ballesteros <johnshift.dev@gmail.com>
2023-01-24 15:08:29 +0800

    refactor(msw): mock job-listing graphql response

---
+++ src/core/types/job-listings.ts
@@ -0,0 +1,42 @@
+import type { MinMax } from './common';
+
+export type Job = {
+  id: number; // ??? need backend info
+  title: string;
+  role: string;
+  salary: MinMax;
+  strat: string;
+  teamSize: number;
+  tz: string;
+  created: string; // ??? backend formatted?
+  tags: { name: string; isChecked: boolean }[];
+  companyId: number; // ?? need backend info
+};
+
+export type Company = {
+  id: number; // ??? need backend info
+  name: string;
+  avatar: string;
+  location: string;
+  teamSize: MinMax;
+  dateFunding: string; // ??? backend formatted?
+  description: string;
+  tags: string[];
+
+  /** List of developers related to company (avatar url array for now) */
+  developers: string[];
+
+  /** Company id of competitors */
+  competitors: number[];
+};
+
+export type JobListingsQuery = {
+  /** All (page-limit) available jobs */
+  jobs: Job[];
+
+  /** List of companies involved */
+  companies: Company[];
+
+  /** Provide total count incase we use pagination */
+  totalCount: number; // ??? need backend info
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,45 @@
-const Home = () => <h1 className="text-3xl font-bold underline">TODO</h1>;
+import { gql, useQuery } from '@apollo/client';
+
+import { JobListingsQuery } from '~/core/types';
+
+const QUERY_JOB_LISTINGS = gql`
+  query JobListings {
+    jobs
+    companies
+    totalCount
+  }
+`;
+
+const Home = () => {
+  const { loading, error, data } =
+    useQuery<JobListingsQuery>(QUERY_JOB_LISTINGS);
+
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  if (!data) return <p>No data returned</p>;
+
+  return (
+    <>
+      <div>
+        <h1 className="pb-4 text-xl">Jobs ({data.totalCount})</h1>
+
+        {data.jobs.map((job) => (
+          <div key={job.id} className="pb-2">
+            <p>job = {JSON.stringify(job, undefined, 2)}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h1 className="pb-4 text-xl">Companies:</h1>
+
+        {data.companies.map((company) => (
+          <div key={company.id} className="pb-2">
+            <p>company = {JSON.stringify(company, undefined, 2)}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Home;
