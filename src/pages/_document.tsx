1a934136e14625cfe61215fab44cd1e71477081e
John Ballesteros <johnshift.dev@gmail.com>
2023-01-25 21:33:30 +0800

    style: add global bg and text-color class-names

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 const Document = () => (
   <Html lang="en">
     <Head />
-    <body>
+    <body className="bg-[#161618] text-[#F9FAFB]">
       <Main />
       <NextScript />
     </body>
