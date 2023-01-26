7aca52c7b0f5fd3a0e0a285ca22387f31045c018
John Ballesteros <johnshift.dev@gmail.com>
2023-01-26 14:07:25 +0800

    refactor: temporarily removing apollo-client since data will be SSR (mocked for now)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,8 @@ import '~/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
-import { ApolloProvider } from '@apollo/client';
-
-import { client } from '~/graphql/apollo-client';
-
-if (
-  process.env.NODE_ENV === 'development' &&
-  typeof window !== 'undefined' &&
-  process.env.NEXT_PUBLIC_API_MOCKING === 'yes'
-) {
-  const { worker } = require('~/mocks/browser');
-  worker.start();
-}
-
 const App = ({ Component, pageProps }: AppProps) => (
-  <ApolloProvider client={client}>
-    <Component {...pageProps} />
-  </ApolloProvider>
+  <Component {...pageProps} />
 );
 
 export default App;
