a16e4706886de383a56165c49f73d2e19d07c446
John Ballesteros <johnshift.dev@gmail.com>
2023-01-26 13:55:56 +0800

    refactor(pages): update dummy pages routes to match route signature

---
+++ src/pages/organizations/[id]/index.tsx
@@ -0,0 +1,25 @@
+import RightPanel from '~/features/right-panel';
+import { SideBar } from '~/features/sidebar';
+import { useRouteSegments } from '~/hooks/use-route-segments';
+import { GenericLayout } from '~/layouts/generic-layout';
+
+const OrganizationsPage = () => {
+  const { pathname, push, section, idSegment, tabSegment } = useRouteSegments();
+  return (
+    <GenericLayout
+      sideBar={<SideBar pathname={pathname} push={push} />}
+      rightPanel={
+        <RightPanel
+          section={section}
+          idSegment={idSegment}
+          tabSegment={tabSegment}
+          push={push}
+        />
+      }
+    >
+      <span className="text-2xl">TODO</span>
+    </GenericLayout>
+  );
+};
+
+export default OrganizationsPage;
