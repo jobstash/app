bd2cd6ad972f287328166c0665f4c3e5492872b3
John Ballesteros <johnshift.dev@gmail.com>
2023-01-27 14:22:25 +0800

    refactor(pages): update pages to match new types + data

--- src/pages/repositories/[id]/index.tsx
+++ src/pages/repositories/[id]/index.tsx
@@ -1,19 +1,23 @@
 import RightPanel from '~/features/right-panel';
 import { SideBar } from '~/features/sidebar';
+import { useRootContext } from '~/hooks/use-root-context';
 import { useRouteSegments } from '~/hooks/use-route-segments';
 import { GenericLayout } from '~/layouts/generic-layout';
 
 const OrganizationsPage = () => {
-  const { pathname, push, section, idSegment, tabSegment } = useRouteSegments();
+  const { segments, push } = useRouteSegments();
+  const { activeIds } = useRootContext();
 
   return (
     <GenericLayout
-      sideBar={<SideBar pathname={pathname} push={push} />}
+      sideBar={
+        <SideBar section={segments.section} push={push} activeIds={activeIds} />
+      }
       rightPanel={
         <RightPanel
-          section={section}
-          idSegment={idSegment}
-          tabSegment={tabSegment}
+          section={segments.section}
+          idSegment={segments.id}
+          tabSegment={segments.tab}
           push={push}
         />
       }
