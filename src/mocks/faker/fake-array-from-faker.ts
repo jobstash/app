b459a7fea0283ccb9fd8dec04c1ecf83ad69b739
John Ballesteros <johnshift.dev@gmail.com>
2023-01-28 22:07:50 +0800

    fix: duplicate keys on mocked data

--- src/mocks/faker/fake-array-from-faker.ts
+++ src/mocks/faker/fake-array-from-faker.ts
@@ -3,7 +3,13 @@ import { faker } from '@faker-js/faker';
 import { VoidFn } from '~/core/types';
 
 /** Returns array using faker fn */
-export const fakeArrayFromFaker = <T>(fn: VoidFn, min: number, max: number) =>
-  Array.from({ length: faker.datatype.number({ min, max }) })
+export const fakeArrayFromFaker = <T>(fn: VoidFn, min: number, max: number) => {
+  const result = Array.from({ length: faker.datatype.number({ min, max }) })
     .fill(0)
     .map(() => fn()) as T[];
+
+  // Only 70% of total length
+  const length = Math.floor(result.length * 0.7);
+
+  return faker.helpers.uniqueArray(result, length);
+};
