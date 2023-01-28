e7d74adf4dab3ade80ae6ce5578e08983eb5e8e7
John Ballesteros <johnshift.dev@gmail.com>
2023-01-29 07:09:25 +0800

    refactor(ui): add `UNSTYLED` `LogoTitle` component

---
+++ src/features/unstyled-ui/logo-title.stories.tsx
@@ -0,0 +1,25 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { LogoTitle } from './logo-title';
+
+export default {
+  component: LogoTitle,
+  title: 'UNSTYLED/base/LogoTitle',
+  // Default Args
+  args: {
+    name: 'Hello World',
+    avatar: `https://api.dicebear.com/5.x/bottts/svg?seed=${Date.now()}`,
+    size: 'lg',
+  },
+} as ComponentMeta<typeof LogoTitle>;
+
+const Template: ComponentStory<typeof LogoTitle> = (args) => (
+  <LogoTitle {...args} />
+);
+
+export const Default = Template.bind({});
+
+export const WithLocation = Template.bind({});
+WithLocation.args = {
+  location: 'Somewhere, Earth',
+};
