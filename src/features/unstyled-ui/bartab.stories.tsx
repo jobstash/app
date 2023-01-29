446ed7eca7ba4076a473c391ae18ba88aed28280
John Ballesteros <johnshift.dev@gmail.com>
2023-01-29 16:58:34 +0800

    refactor(ui): add `UNSTYLED` `Bartab` component

---
+++ src/features/unstyled-ui/bartab.stories.tsx
@@ -0,0 +1,45 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Avatar } from './base/avatar';
+import { Bartab } from './bartab';
+import { CodeSidebarIcon } from './icons';
+
+export default {
+  component: Bartab,
+  title: 'UNSTYLED/base/Bartab',
+  // Default Args
+  args: {
+    text: 'Continue with Github',
+  },
+} as ComponentMeta<typeof Bartab>;
+
+const Template: ComponentStory<typeof Bartab> = (args) => (
+  <div className="w-52">
+    <Bartab {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+
+export const IsActive = Template.bind({});
+IsActive.args = {
+  isActive: true,
+};
+
+export const HasLeft = Template.bind({});
+HasLeft.args = {
+  text: 'Jobs',
+  left: <CodeSidebarIcon />,
+};
+
+export const HasAvatar = Template.bind({});
+HasAvatar.args = {
+  text: '@0xDevoor',
+  left: (
+    <Avatar
+      src={`https://api.dicebear.com/5.x/bottts/svg?seed=${Date.now()}`}
+      size="xs"
+      alt="Test"
+    />
+  ),
+};
