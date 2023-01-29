8e467b1e329b5e83a1105210166bdf2f2ca631ef
John Ballesteros <johnshift.dev@gmail.com>
2023-01-29 16:58:19 +0800

    refactor(ui): add `UNSTYLED` `TechWrapper` component

---
+++ src/features/unstyled-ui/tech-wrapper.stories.tsx
@@ -0,0 +1,24 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { TechWrapper } from './tech-wrapper';
+
+export default {
+  component: TechWrapper,
+  title: 'UNSTYLED/base/TechWrapper',
+  // Default args
+  args: {
+    text: 'webgl',
+    isParentActive: false,
+  },
+} as ComponentMeta<typeof TechWrapper>;
+
+const Template: ComponentStory<typeof TechWrapper> = (args) => (
+  <TechWrapper {...args} />
+);
+
+export const Default = Template.bind({});
+
+export const IsChecked = Template.bind({});
+IsChecked.args = {
+  isChecked: true,
+};
