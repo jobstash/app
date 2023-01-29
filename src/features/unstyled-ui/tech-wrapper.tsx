824c21d6dbca56e70c2474acbb2d30eb4b190cb7
John Ballesteros <johnshift.dev@gmail.com>
2023-01-29 16:58:19 +0800

    refactor(ui): add `UNSTYLED` `TechWrapper` component

---
+++ src/features/unstyled-ui/tech-wrapper.tsx
@@ -0,0 +1,72 @@
+import { type ButtonHTMLAttributes, MouseEventHandler, useState } from 'react';
+
+import { type VariantProps, cva } from 'class-variance-authority';
+import clsx from 'clsx';
+
+import { Text } from './base/text';
+import { AddIcon, CheckIcon } from './icons';
+
+const cvaTechWrapper = cva([
+  'border border-tech px-1.5 rounded-sm relative',
+  'active:scale-[.95] transition duration-150 ease-in-out',
+]);
+
+type TechWrapperVariantProps = VariantProps<typeof cvaTechWrapper>;
+
+interface TechWrapperProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    TechWrapperVariantProps {
+  /** Tech text */
+  text: string;
+
+  /** Techs can either be */
+  isChecked: boolean;
+
+  /** Temporary since text-tech is not visible when card is active */
+  isParentActive?: boolean;
+}
+
+export const TechWrapper = ({
+  text,
+  isChecked,
+  isParentActive = false,
+  ...props
+}: TechWrapperProps) => {
+  const [isCheckedState, setIsCheckedState] = useState(isChecked);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const displayIndicator = isCheckedState || isHovering;
+
+  // Just toggle is-match state for now
+  const onClick: MouseEventHandler = (e) => {
+    e.stopPropagation();
+    setIsCheckedState((prev) => !prev);
+  };
+
+  return (
+    <button
+      type="button"
+      className={cvaTechWrapper()}
+      onClick={onClick}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+      {...props}
+    >
+      <Text
+        className={clsx('text-tech', {
+          'text-white/80': isParentActive,
+        })}
+      >
+        {text.toUpperCase()}
+      </Text>
+      {displayIndicator && (
+        <div className="absolute -top-2 -right-3 h-5 w-5 items-center justify-center">
+          <div className="relative">
+            {isCheckedState && <CheckIcon />}
+            {isHovering && !isCheckedState && <AddIcon />}
+          </div>
+        </div>
+      )}
+    </button>
+  );
+};
