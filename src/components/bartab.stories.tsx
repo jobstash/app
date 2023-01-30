dfcb88ea7e87069c36d14c5f0312bb9e4c041609
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2023-01-30 14:42:14 +0100

    feature(style components): bartab styles

--- src/components/bartab.stories.tsx
+++ src/components/bartab.stories.tsx
@@ -1,29 +1,47 @@
-import React from 'react';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { Story } from '@storybook/react';
-
-import { Bartab, BartabProps } from '../components/bartab';
+import { Avatar } from './base/avatar';
+import { Bartab } from './bartab';
+import { CodeSidebarIcon } from './icons';
 
 export default {
-  title: 'Components/Bartab',
   component: Bartab,
-};
+  title: 'components/base/Bartab',
+  // Default Args
+  args: {
+    text: 'Continue with Github',
+  },
+} as ComponentMeta<typeof Bartab>;
 
-const Template: Story<BartabProps> = (args) => <Bartab {...args} />;
-
-export const Link = Template.bind({});
-Link.args = {
-  children: 'Bartab component',
-};
-
-export const Tab = () => (
-  <Bartab variant="link" leftSection={undefined}>
-    hello
-  </Bartab>
+const Template: ComponentStory<typeof Bartab> = (args) => (
+  <div className="w-full">
+    <Bartab {...args} />
+  </div>
 );
 
-export const Profile = () => (
-  <Bartab variant="link" leftSection={undefined}>
-    hello
-  </Bartab>
-);
+export const Default = Template.bind({});
+
+export const IsActive = Template.bind({});
+IsActive.args = {
+  isActive: true,
+};
+
+export const HasLeft = Template.bind({});
+HasLeft.args = {
+  text: 'Jobs',
+  intent: "secondary",
+  left: <CodeSidebarIcon />,
+};
+
+export const HasAvatar = Template.bind({});
+HasAvatar.args = {
+  text: '@0xDevoor',
+  intent: "secondary",
+  left: (
+    <Avatar
+      src={`https://api.dicebear.com/5.x/bottts/svg?seed=${Date.now()}`}
+      size="xs"
+      alt="Test"
+    />
+  ),
+};
