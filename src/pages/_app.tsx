d207edc4f80834f91a77b5d886089c9152e2ef0f
John Ballesteros <johnshift.dev@gmail.com>
2023-02-02 15:05:14 +0800

    refactor(style): implement `Lato` font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,21 @@ import '~/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+import { Lato } from '@next/font/google';
+
+const lato = Lato({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-lato',
+});
+
 import { RootProvider } from '~/contexts/root-context';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <RootProvider>
-    <Component {...pageProps} />
+    <div className={`${lato.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
   </RootProvider>
 );
 
