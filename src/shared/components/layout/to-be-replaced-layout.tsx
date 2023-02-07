48d7dc58a28ae91bf2e620819cd750fc9406f922
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2023-02-07 17:09:47 +0100

    feature: layout grid

--- src/shared/components/layout/to-be-replaced-layout.tsx
+++ src/shared/components/layout/to-be-replaced-layout.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable tailwindcss/no-arbitrary-value */
-
 import { ReactNode } from 'react';
 
 interface Props {
@@ -7,23 +5,25 @@ interface Props {
   rightPanel: ReactNode;
   children: ReactNode;
 }
+// eslint-disable-next-line no-warning-comments
+// TODO: Rework this background
+const style = {
+  background:
+    'linear-gradient(116.16deg, #141317 3.32%, rgba(18, 18, 22, 0.7) 96.7%)',
+};
 
 export const ToBeReplacedLayout = ({
   sidebar,
   children,
   rightPanel,
 }: Props) => (
-  <div className="flex">
-    <section className="relative w-[14%] flex-col border-r border-r-zinc-800">
-      {sidebar}
-    </section>
-    <div className="flex grow justify-center">
-      <div className="flex w-[95%] max-w-3xl pt-8">
-        <div className="flex w-full flex-col space-y-16">{children}</div>
-      </div>
+  <div className="flex flex-wrap pl-52" style={style}>
+    {sidebar}
+    <div className="flex w-2/3 flex-1 justify-center p-6">
+      <div className="w-full space-y-8">{children}</div>
     </div>
-    <aside className="w-[40%] max-w-2xl border-l border-zinc-800">
-      <div className="hide-scrollbar sticky top-0 max-h-screen space-y-6 overflow-y-scroll px-6">
+    <aside className="w-1/3 shrink-0">
+      <div className="hide-scrollbar sticky top-0 max-h-screen space-y-6 overflow-y-scroll bg-white/5 p-6">
         {rightPanel}
       </div>
     </aside>
