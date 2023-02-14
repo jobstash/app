3398f37c1d349e7232eb31b0f1b28d612777b7bc
cristiano <cristiano@welance.com>
2023-02-14 17:06:05 +0100

    Repository Card Styles (#26)
    * fix(tailwind config): color variables
    
    * feature(Job Card): first styles
    
    * feature(repo card): first styles
    
    ---------
    
    Co-authored-by: cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>

--- src/features/repos/components/repo-card.tsx
+++ src/features/repos/components/repo-card.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image';
 import type { MouseEventHandler } from 'react';
+
+import { cva } from 'class-variance-authority';
 
 import type { RepoPost } from '~/core/interfaces';
 import { Button } from '~/shared/components';
 
 import { createRepoTags, getRepoTechs } from '../utils';
+
+const cvaRepoCard = cva(
+  [
+    'w-full space-y-4 overflow-hidden rounded-3xl bg-white/5 p-5 text-ivory cursor-pointer',
+  ],
+  {
+    variants: {
+      isActive: {
+        true: 'bg-gradient-to-l from-primary to-secondary',
+      },
+    },
+  },
+);
+
 
 interface Props {
   post: RepoPost;
@@ -20,43 +37,42 @@ export const RepoCard = ({ post, isActive, onClick }: Props) => {
   const techs = getRepoTechs(details);
 
   return (
-    <div onClick={onClick}>
-      <div>
-        <p>{name}</p>
-        <div>
-          <p>{created}</p>
+    <div className={cvaRepoCard({ isActive })} onClick={onClick}>
+      <div className='flex items-center justify-between'>
+        <h2 className='text-xl font-medium'>{name}</h2>
+        <div className='flex items-center space-x-2'>
+          <span className='text-sm'>{created}</span>
           <Button>bookmark</Button>
         </div>
       </div>
-
-      <div>
-        <h3>{description}</h3>
-      </div>
-
-      <div>
+      <p className='text-sm'>{description}</p>
+      <div className='flex space-x-8 border-b border-white/5 pb-4 text-sm'>
         {tags.map((tag) => (
-          <div key={tag.text}>
-            {tag.icon}
+          <div key={tag.text} className='flex items-center'>
+            <div className='mr-2'>{tag.icon}</div>
             <p>{tag.text}</p>
             <p>{tag.link}</p>
           </div>
         ))}
       </div>
 
-      <hr />
+      <div className='flex space-x-4'>
+        {techs.map((tech) => (
+          <div key={tech.name} className='relative flex self-start rounded-sm border border-white	p-1'>
+            <span className='text-sm font-semibold'>{tech.name}</span>
+            <div className='absolute right-0 top-0 -mt-2 -mr-2 h-4 w-4 rounded-full bg-white'>{tech.isChecked}</div>
+          </div>
+        ))}
+      </div>
 
-      {techs.map((tech) => (
-        <div key={tech.name}>
-          <p>{tech.name}</p>
-          <p>{tech.isChecked}</p>
-        </div>
-      ))}
-
-      <hr />
-
-      <div>
-        <p>{org.name}</p>
-        <p>{org.avatar}</p>
+      <div className='flex items-center space-x-4 border-t border-white/5 pt-4'>
+        <Image
+          src={org.avatar}
+          width="32"
+          height="32"
+          alt={org.name}
+        />
+        <h3 className='font-semibold'>{org.name}</h3>
       </div>
     </div>
   );
