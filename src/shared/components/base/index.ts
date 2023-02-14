5941b8713681d689be68b74188ece02dda84b529
John Ballesteros <johnshift.dev@gmail.com>
2023-02-14 11:42:47 +0800

    refactor(components): add base range-slider component

--- src/shared/components/base/index.ts
+++ src/shared/components/base/index.ts
@@ -3,5 +3,6 @@ export * from './bartab';
 export * from './brand';
 export * from './button';
 export * from './dropdown';
+export * from './range-slider';
 export * from './select';
 export * from './text';
