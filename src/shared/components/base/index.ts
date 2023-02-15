4a1bba1c99fc5604a856997d9a6756c9c793946d
John Ballesteros <johnshift.dev@gmail.com>
2023-02-15 20:05:12 +0800

    refactor(components): remove base dropdown component

--- src/shared/components/base/index.ts
+++ src/shared/components/base/index.ts
@@ -2,7 +2,5 @@ export * from './avatar';
 export * from './bartab';
 export * from './brand';
 export * from './button';
-export * from './dropdown';
 export * from './range-slider';
-export * from './select';
 export * from './text';
