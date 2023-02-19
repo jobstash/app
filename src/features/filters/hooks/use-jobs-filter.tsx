a79881f524d18c038922972b6928c95edab1b2ae
John Ballesteros <johnshift.dev@gmail.com>
2023-02-19 21:18:50 +0800

    refactor(filters): moved use-jobs-filter hook fetchedConfig to args

--- src/features/filters/hooks/use-jobs-filter.tsx
+++ src/features/filters/hooks/use-jobs-filter.tsx
@@ -1,4 +1,4 @@
-import { type Reducer, ReactNode, useMemo, useReducer } from 'react';
+import { type ReactNode, type Reducer, useMemo, useReducer } from 'react';
 
 import { MultiSelectFilter } from '../components/multi-select-filter';
 import { MultiSelectSearchFilter } from '../components/multi-select-search-filter';
@@ -17,26 +17,24 @@ import {
   getSingleSelectProps,
 } from '../utils';
 
-import { sampleFilterConfig } from './sample-config';
-
 type ShownSortedConfig = {
   key: keyof FilterState;
   config: JobsFilterConfig[keyof JobsFilterConfig];
 };
 
-export const useJobsFilter = () => {
-  const fetchedConfig = sampleFilterConfig;
-
+export const useJobsFilter = (fetchedConfig?: JobsFilterConfig) => {
   // Filter only shown configs then sort - `useMemo` for perf
   const shownSortedConfigs: ShownSortedConfig[] = useMemo(
     () =>
-      Object.entries(fetchedConfig)
-        .filter(([_, config]) => config.show)
-        .sort(([_k1, v1], [_k2, v2]) => v1.position - v2.position)
-        .map(([key, config]) => ({
-          key: key as keyof FilterState,
-          config,
-        })),
+      fetchedConfig
+        ? Object.entries(fetchedConfig)
+            .filter(([_, config]) => config.show)
+            .sort(([_k1, v1], [_k2, v2]) => v1.position - v2.position)
+            .map(([key, config]) => ({
+              key: key as keyof FilterState,
+              config,
+            }))
+        : [],
     [fetchedConfig],
   );
 
@@ -49,82 +47,87 @@ export const useJobsFilter = () => {
   const filterComponents: { key: keyof JobsFilterConfig; ui: ReactNode }[] =
     useMemo(
       () =>
-        shownSortedConfigs.map(({ key, config }) => {
-          const { kind } = config;
+        fetchedConfig
+          ? shownSortedConfigs.map(({ key, config }) => {
+              const { kind } = config;
 
-          switch (kind) {
-            case FilterKind.DATE:
-            case FilterKind.BOOLEAN:
-            case FilterKind.SINGLESELECT: {
-              const { text, labels, ariaLabel } = getSingleSelectProps(config);
+              switch (kind) {
+                case FilterKind.DATE:
+                case FilterKind.BOOLEAN:
+                case FilterKind.SINGLESELECT: {
+                  const { text, labels, ariaLabel } =
+                    getSingleSelectProps(config);
 
-              return {
-                key,
-                ui: (
-                  <SingleSelectFilter
-                    type={key}
-                    dispatch={dispatch}
-                    text={text}
-                    labels={labels}
-                    ariaLabel={ariaLabel}
-                  />
-                ),
-              };
-            }
+                  return {
+                    key,
+                    ui: (
+                      <SingleSelectFilter
+                        type={key}
+                        dispatch={dispatch}
+                        text={text}
+                        labels={labels}
+                        ariaLabel={ariaLabel}
+                      />
+                    ),
+                  };
+                }
 
-            case FilterKind.RANGE: {
-              const { text, range } = getRangeProps(filters, key, config);
+                case FilterKind.RANGE: {
+                  const { text, range } = getRangeProps(filters, key, config);
 
-              return {
-                key,
-                ui: (
-                  <RangeFilter
-                    text={text}
-                    range={range}
-                    type={key}
-                    dispatch={dispatch}
-                  />
-                ),
-              };
-            }
+                  return {
+                    key,
+                    ui: (
+                      <RangeFilter
+                        text={text}
+                        range={range}
+                        type={key}
+                        dispatch={dispatch}
+                      />
+                    ),
+                  };
+                }
 
-            case FilterKind.MULTISELECT:
-            case FilterKind.MULTISELECT_SEARCH: {
-              const { text, items, selectedItems } = getMultiSelectProps(
-                filters,
-                key,
-                config,
-              );
-              const hasSearch = kind === FilterKind.MULTISELECT_SEARCH;
+                case FilterKind.MULTISELECT:
+                case FilterKind.MULTISELECT_SEARCH: {
+                  const { text, items, selectedItems } = getMultiSelectProps(
+                    filters,
+                    key,
+                    config,
+                  );
+                  const hasSearch = kind === FilterKind.MULTISELECT_SEARCH;
 
-              return {
-                key,
-                ui: hasSearch ? (
-                  <MultiSelectSearchFilter
-                    type={key}
-                    dispatch={dispatch}
-                    text={text}
-                    items={items}
-                    selectedItems={selectedItems}
-                  />
-                ) : (
-                  <MultiSelectFilter
-                    type={key}
-                    dispatch={dispatch}
-                    text={text}
-                    items={items}
-                    selectedItems={selectedItems}
-                  />
-                ),
-              };
-            }
+                  return {
+                    key,
+                    ui: hasSearch ? (
+                      <MultiSelectSearchFilter
+                        type={key}
+                        dispatch={dispatch}
+                        text={text}
+                        items={items}
+                        selectedItems={selectedItems}
+                      />
+                    ) : (
+                      <MultiSelectFilter
+                        type={key}
+                        dispatch={dispatch}
+                        text={text}
+                        items={items}
+                        selectedItems={selectedItems}
+                      />
+                    ),
+                  };
+                }
 
-            default: {
-              throw new Error(`Unrecognized filter component: kind=${kind}`);
-            }
-          }
-        }),
-      [filters, shownSortedConfigs],
+                default: {
+                  throw new Error(
+                    `Unrecognized filter component: kind=${kind}`,
+                  );
+                }
+              }
+            })
+          : [],
+      [fetchedConfig, filters, shownSortedConfigs],
     );
 
   return { filters, filterComponents };
