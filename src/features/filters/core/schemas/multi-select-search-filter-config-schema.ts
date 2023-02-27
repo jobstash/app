32333bc597f9953cf835e679e41be175092e1385
John Ballesteros <johnshift.dev@gmail.com>
2023-02-27 13:34:18 +0800

    refactor(filters): update schemas to match core definitions

--- src/features/filters/core/schemas/multi-select-search-filter-config-schema.ts
+++ src/features/filters/core/schemas/multi-select-search-filter-config-schema.ts
@@ -1,16 +1,16 @@
 import Joi from 'joi';
 
-import { FilterKind } from '../constants';
-import type { MultiSelectSearchFilterConfig } from '../types';
+import { FILTER_KIND_MULTISELECT_WITH_SEARCH } from '../constants';
+import type { MultiSelectSearchFilterConfig } from '../interfaces';
 
 import { FilterConfigSharedPropertiesSchema } from './filter-config-shared-properties-schema';
 import { ParamKeySchema } from './param-key-schema';
 
 export const MultiSelectSearchFilterConfigSchema =
   FilterConfigSharedPropertiesSchema.append<MultiSelectSearchFilterConfig>({
-    kind: Joi.number().valid(FilterKind.MULTISELECT_SEARCH).required(),
-    param_key: ParamKeySchema,
-    value: Joi.array()
+    kind: Joi.string().valid(FILTER_KIND_MULTISELECT_WITH_SEARCH).required(),
+    paramKey: ParamKeySchema,
+    options: Joi.array()
       .items(
         Joi.object({
           label: Joi.string().required(),
