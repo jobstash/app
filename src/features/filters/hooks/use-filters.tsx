58bfa21b0accd423e3de38d9aaa13ce687984b4c
John Ballesteros <johnshift.dev@gmail.com>
2023-03-08 15:03:53 +0800

    refactor(filters): update ignored filter-configs due to recent changes
    complete RIP-239

--- src/features/filters/hooks/use-filters.tsx
+++ src/features/filters/hooks/use-filters.tsx
@@ -37,15 +37,15 @@ type ShownSortedConfig = {
 
 // TODO: Remove once mw fixed returned values
 const ignoredFilterConfigs = new Set([
-  KEY_PUBLICATION_DATE, // Should be SINGLESELECT
-  KEY_SALARY, // Step is 0
-  KEY_TEAM_SIZE, // Range has null values
-  KEY_HEAD_COUNT, // Range has null values
-  KEY_AUDITS, // Weird fields: value.{highest, lowest}.value.{low, high}
-  KEY_HACKS, // Weird fields: value.{highest, lowest}.value.{low, high}
-  KEY_SENIORITY, // Getting "seniority" must be a postive number error
-  KEY_MAINNET, // Should be SINGLESELECT
-  KEY_TOKEN, // Should be SINGLESELECT
+  // KEY_PUBLICATION_DATE, // Should be SINGLESELECT
+  KEY_SALARY, // Internal error
+  KEY_TEAM_SIZE, // Range has 0 min/max values
+  KEY_HEAD_COUNT, // Range has 0 min/max values
+  KEY_AUDITS, // Range has 0 min/max values
+  KEY_HACKS, // Not a range -> min = 0, and range = 1, stepSize = 5
+  KEY_SENIORITY, // Errors "seniority must be a postive number error", "seniority must be a number conforming to the specified constraints"
+  KEY_MAINNET, // Internal error
+  // KEY_TOKEN, // Should be SINGLESELECT
   KEY_LOCATIONS, // No required "label" field, No "paramKey" field
   // KEY_TECH, // No required "label" field, empty string in options
   // KEY_ORGANIZATIONS, // No required "label" field
