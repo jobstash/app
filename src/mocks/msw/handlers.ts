f2f4d0f5856411b776aa454c5df76616478c5819
John Ballesteros <johnshift.dev@gmail.com>
2023-03-08 09:55:07 +0800

    chore: cleanup unused filter-config (mw returns filters now)

--- src/mocks/msw/handlers.ts
+++ src/mocks/msw/handlers.ts
@@ -1,14 +1,1 @@
-import { rest } from 'msw';
-
-import { fakeFilterConfig } from '~/features/filters/testutils';
-import { API_MW_URL } from '~/shared/core/constants';
-
-export const handlers = [
-  // // GET jobs filter config
-  // rest.get(`${API_MW_URL}/jobs/filters`, async (_req, res, ctx) => {
-  //   // Artificial delay
-  //   // eslint-disable-next-line no-promise-executor-return
-  //   await new Promise((r) => setTimeout(r, 1000));
-  //   return res(ctx.status(200), ctx.json(fakeFilterConfig()));
-  // }),
-];
+export const handlers = [];
