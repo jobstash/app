e732c070a7c5b0e3de8a15d9cb9421c1aa5565df
John Ballesteros <johnshift.dev@gmail.com>
2023-03-16 20:59:49 +0800

    refactor: add basic auth

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  matcher: [
+    '/',
+    '/jobs/(.*)',
+    '/organizations/(.*)',
+    '/projects/(.*)',
+    '/repositories/(.*)',
+  ],
+};
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+  const url = req.nextUrl;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === 'gotrekt' && pwd === 'mcdonalds') {
+      return NextResponse.next();
+    }
+  }
+
+  url.pathname = '/api/auth';
+
+  return NextResponse.rewrite(url);
+}
