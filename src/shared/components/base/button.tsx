d7e6fa465b41d7044e93bdd0a9017cbc700e9944
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2023-04-18 16:05:50 +0200

    refactor(buttons): animation hover primary button

--- src/shared/components/base/button.tsx
+++ src/shared/components/base/button.tsx
@@ -15,7 +15,7 @@ const wrapper = cva(
         default:
           'bg-dark-gray hover:bg-gray active:bg-gradient-to-l active:from-primary active:to-tertiary active:border-transparent [&>*]:active:bg-dark-gray',
         primary:
-          'bg-gradient-to-l from-primary to-tertiary hover:from-[#8743FF_52.6%] hover:to-[#4136F1_84.9%] active:from-secondary active:to-secondary [&>*]:active:bg-secondary',
+          'relative bg-gradient-to-l from-primary to-tertiary z-10 before:duration-200 before:ease-linear before:transition-all before:z-0 before:block before:absolute before:inset-0 before:content-[""] before:bg-gradient-to-l before:from-primary before:from-55% before:to-tertiary before:to-85%  before:opacity-0 before:z-0 before:rounded-lg hover:before:opacity-100 active:before:opacity-0 active:from-secondary active:to-secondary',
         outline: 'bg-dark hover:bg-dark-gray [&>*]:active:bg-dark',
         subtle: 'bg-dark hover:bg-dark-gray [&>*]:active:bg-dark',
         translucent: 'bg-white/10 hover:bg-white/20 active:bg-white/30',
@@ -52,67 +52,70 @@ const wrapper = cva(
   },
 );
 
-const button = cva(['flex items-center justify-center rounded-lg gap-x-1'], {
-  variants: {
-    variant: {
-      default: '',
-      primary: '',
-      outline: 'ring-1 ring-gray active:ring-0',
-      subtle: '',
-      translucent: '',
+const button = cva(
+  ['flex items-center justify-center z-20 relative rounded-lg gap-x-1'],
+  {
+    variants: {
+      variant: {
+        default: '',
+        primary: '',
+        outline: 'ring-1 ring-gray active:ring-0',
+        subtle: '',
+        translucent: '',
+      },
+      size: {
+        sm: 'px-2 py-1',
+        md: 'px-4 py-2',
+      },
+      isActive: {
+        true: '',
+      },
+      isDisabled: {
+        true: '',
+      },
+      hasLeft: {
+        true: '',
+      },
+      hasRight: {
+        true: '',
+      },
+      isIcon: {
+        true: 'w-7 h-7',
+      },
+      isFullWidth: {
+        true: 'w-full justify-between',
+      },
     },
-    size: {
-      sm: 'px-2 py-1',
-      md: 'px-4 py-2',
-    },
-    isActive: {
-      true: '',
-    },
-    isDisabled: {
-      true: '',
-    },
-    hasLeft: {
-      true: '',
-    },
-    hasRight: {
-      true: '',
-    },
-    isIcon: {
-      true: 'w-7 h-7',
-    },
-    isFullWidth: {
-      true: 'w-full justify-between',
-    },
-  },
 
-  compoundVariants: [
-    {
-      variant: 'outline',
-      isActive: true,
-      className: 'ring-0 ring-transparent hover:bg-dark-gray',
-    },
-    {
-      hasLeft: true,
-      size: 'md',
-      className: 'pl-2',
-    },
-    {
-      hasLeft: true,
-      size: 'sm',
-      className: 'pl-1',
-    },
-    {
-      hasRight: true,
-      size: 'md',
-      className: 'pr-2',
-    },
-    {
-      hasRight: true,
-      size: 'sm',
-      className: 'pr-1',
-    },
-  ],
-});
+    compoundVariants: [
+      {
+        variant: 'outline',
+        isActive: true,
+        className: 'ring-0 ring-transparent hover:bg-dark-gray',
+      },
+      {
+        hasLeft: true,
+        size: 'md',
+        className: 'pl-2',
+      },
+      {
+        hasLeft: true,
+        size: 'sm',
+        className: 'pl-1',
+      },
+      {
+        hasRight: true,
+        size: 'md',
+        className: 'pr-2',
+      },
+      {
+        hasRight: true,
+        size: 'sm',
+        className: 'pr-1',
+      },
+    ],
+  },
+);
 
 type ButtonVariantProps = VariantProps<typeof button>;
 
