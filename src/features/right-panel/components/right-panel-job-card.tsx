6278ba8dc6e545dda2ceffbeede195439eef38a1
John Ballesteros <johnshift.dev@gmail.com>
2023-04-22 10:48:51 +0800

    refactor: only show right-panel techs when there're techs

--- src/features/right-panel/components/right-panel-job-card.tsx
+++ src/features/right-panel/components/right-panel-job-card.tsx
@@ -75,29 +75,33 @@ export const RightPanelJobCard = ({ job, technologies }: Props) => {
         </div>
       ))}
 
-      <div className="flex h-8 flex-col justify-center">
-        <hr className="border-t border-white/10" />
-      </div>
+      {technologies.length > 0 && (
+        <>
+          <div className="flex h-8 flex-col justify-center">
+            <hr className="border-t border-white/10" />
+          </div>
 
-      <div className="flex flex-col gap-5">
-        <div className="flex flex-col gap-2">
-          <Heading size="sm" fw="semibold">
-            Technologies
-          </Heading>
-          <Text color="dimmed">
-            Uncover the technical skills and tools employed by the company, and
-            gain insight into the technologies that drive their success.
-          </Text>
-        </div>
-      </div>
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-2">
+              <Heading size="sm" fw="semibold">
+                Technologies
+              </Heading>
+              <Text color="dimmed">
+                Uncover the technical skills and tools employed by the company,
+                and gain insight into the technologies that drive their success.
+              </Text>
+            </div>
+          </div>
 
-      <div className="flex flex-wrap gap-3">
-        {technologies.map((tech) => (
-          <TechWrapper key={tech.id} id={tech.id}>
-            {tech.name}
-          </TechWrapper>
-        ))}
-      </div>
+          <div className="flex flex-wrap gap-3">
+            {technologies.map((tech) => (
+              <TechWrapper key={tech.id} id={tech.id}>
+                {tech.name}
+              </TechWrapper>
+            ))}
+          </div>
+        </>
+      )}
 
       {/* <Button variant="primary">Explore Project</Button> */}
     </div>
