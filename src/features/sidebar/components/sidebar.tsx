7eec4a87a9ba46948f0df6c39b15e6d69cf22e84
John Ballesteros <johnshift.dev@gmail.com>
2023-04-25 09:34:25 +0800

    refactor: removed alert on jobs bartab, also remove all unused bartabs

--- src/features/sidebar/components/sidebar.tsx
+++ src/features/sidebar/components/sidebar.tsx
@@ -1,56 +1,26 @@
-import { CHECK_WALLET_ROLES } from '~/features/auth/core/constants';
-import { useWalletAuthContext } from '~/features/auth/hooks';
-import { Brand, ConnectWalletButton } from '~/shared/components';
-
 import {
-  bookmarkBartabs,
-  discoverBartabs,
-  roleSectionMap,
-} from '../core/constants';
+  Bartab,
+  Brand,
+  ConnectWalletButton,
+  JobsSidebarIcon,
+  Text,
+} from '~/shared/components';
 
-import { SidebarSection } from './sidebar-section';
-
-export const SideBar = () => {
-  const { role } = useWalletAuthContext();
-
-  const roleSection = roleSectionMap[role];
-
-  return (
-    <nav className="fixed left-0 z-50 flex h-[65px] w-full flex-col bg-gradient-to-l from-[#141317] to-[#121216] p-4 lg:inset-y-0 lg:h-auto lg:min-h-screen lg:w-52 lg:border-r lg:border-white/5 lg:bg-transparent">
-      <div className="lg:p-4 lg:pl-1">
-        <Brand />
-      </div>
-      <div>
-        <SidebarSection
-          title="Discover"
-          tabs={discoverBartabs}
-          isActiveFn={({ tab: { label }, segments: { section } }) =>
-            `${section}` === `${label.toLowerCase()}`
-          }
-        />
-      </div>
-      {role === CHECK_WALLET_ROLES.DEV && (
-        <div>
-          <SidebarSection
-            title="Bookmarked"
-            tabs={bookmarkBartabs}
-            isActiveFn={() => false}
-          />
+export const SideBar = () => (
+  <nav className="fixed left-0 z-50 flex h-[65px] w-full flex-col bg-gradient-to-l from-[#141317] to-[#121216] p-4 lg:inset-y-0 lg:h-auto lg:min-h-screen lg:w-52 lg:border-r lg:border-white/5 lg:bg-transparent">
+    <div className="lg:p-4 lg:pl-1">
+      <Brand />
+    </div>
+    <div>
+      <div className="mt-12 hidden lg:block">
+        <Text color="dimmed">Discover</Text>
+        <div className="space-y-3 pt-3">
+          <Bartab isActive left={<JobsSidebarIcon />} text="Jobs" />
         </div>
-      )}
-      <div className="inset-x-0 bottom-0 hidden space-y-4 p-4 lg:absolute lg:block">
-        {roleSection ? (
-          <SidebarSection
-            title={roleSection.title}
-            tabs={roleSection.tabs}
-            isActiveFn={roleSection.isActiveFn}
-          />
-        ) : null}
-
-        <hr className="border-t border-white/20" />
-
-        <ConnectWalletButton />
       </div>
-    </nav>
-  );
-};
+    </div>
+    <div className="inset-x-0 bottom-0 hidden space-y-4 p-4 lg:absolute lg:block">
+      <ConnectWalletButton />
+    </div>
+  </nav>
+);
