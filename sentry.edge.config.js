65b600211fb50cb1aae5eec4efef36c6b3b84dfb
John Ballesteros <johnshift.dev@gmail.com>
2023-04-25 21:12:33 +0800

    refactor: update sentry with new config

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -7,8 +7,9 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN,
-  environment: 'production',
+  dsn:
+    SENTRY_DSN ||
+    'https://714cca1095c042fda9f542dde5ed7063@o4504495959703552.ingest.sentry.io/4504519276363776',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
   // ...
