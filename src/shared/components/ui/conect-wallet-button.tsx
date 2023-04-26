9f63bd39b75467ef8b05da86e89bef91fc74f31a
John Ballesteros <johnshift.dev@gmail.com>
2023-04-26 14:51:24 +0800

    refactor: show admin text in connect-wallet-button

--- src/shared/components/ui/conect-wallet-button.tsx
+++ src/shared/components/ui/conect-wallet-button.tsx
@@ -1,11 +1,13 @@
 import { Avatar as CkAvatar, ConnectKitButton } from 'connectkit';
 
+import { useWalletAuthContext } from '~/features/auth/hooks';
 import { useIsMounted } from '~/shared/hooks';
 
 import { Bartab } from '../base';
 
 export const ConnectWalletButton = () => {
   const isMounted = useIsMounted();
+  const { role } = useWalletAuthContext();
 
   if (!isMounted)
     return <Bartab variant="wallet" text="Connect Wallet" isActive={false} />;
@@ -24,8 +26,10 @@ export const ConnectWalletButton = () => {
             }
             right={null}
             text={
-              address
-                ? `${address.slice(0, 6)}...${address.slice(-4)}`
+              address && role !== 'ANON'
+                ? role === 'ADMIN'
+                  ? 'ADMIN'
+                  : `${address.slice(0, 6)}...${address.slice(-4)}`
                 : 'Connect Wallet'
             }
             onClick={() => (show ? show() : null)}
