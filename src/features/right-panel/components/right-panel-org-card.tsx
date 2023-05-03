b3b7e4098e85d9bf8825a1aa309e32088908acb4
John Ballesteros <johnshift.dev@gmail.com>
2023-05-03 09:28:44 +0800

    refactor: add dotted lines and subheadings in org right-panel

--- src/features/right-panel/components/right-panel-org-card.tsx
+++ src/features/right-panel/components/right-panel-org-card.tsx
@@ -34,35 +34,64 @@ export const RightPanelOrgCard = ({ org, fundingRounds, investors }: Props) => {
           <Text color="dimmed">{description}</Text>
         </div>
 
-        <div className="flex h-fit flex-col justify-center">
-          <hr className="border-t border-white/10" />
-        </div>
-
-        <div className="flex flex-col justify-center gap-3">
-          {fundingRounds.map(({ id, date, raisedAmount, roundName }) => (
-            <div key={id} className="flex items-center gap-x-3">
-              {roundName && (
-                <CardSet
-                  icon={<BankIcon />}
-                >{`Round Name: ${roundName}`}</CardSet>
-              )}
-              <CardSet icon={<MoneyIcon />}>
-                {`Amount: $${numFormatter.format(raisedAmount)}M`}
-              </CardSet>
-              <CardSet icon={<BankIcon />}>{`Date: ${createOrgFundingDateString(
-                fundingRounds[0].date,
-              )}`}</CardSet>
+        {fundingRounds.length > 0 && (
+          <>
+            <div className="flex h-fit flex-col justify-center">
+              <hr className="border-t border-white/10" />
             </div>
-          ))}
 
-          <div className="flex flex-wrap items-center gap-3">
-            {investors.map(({ name }) => (
-              <CardSet key={name} icon={<MoneyIcon />}>
-                {name}
-              </CardSet>
-            ))}
-          </div>
-        </div>
+            <Heading size="sm" fw="semibold">
+              Funding Rounds
+            </Heading>
+
+            <div className="flex flex-col justify-center gap-4 pl-1">
+              {fundingRounds.map(({ id, date, raisedAmount, roundName }, i) => (
+                <div key={id} className="flex flex-col gap-2.5">
+                  <div className="flex items-center gap-x-4">
+                    {roundName && (
+                      <CardSet
+                        icon={<BankIcon />}
+                      >{`Funding Round: ${roundName}`}</CardSet>
+                    )}
+                    <CardSet
+                      icon={<BankIcon />}
+                    >{`Funding Date: ${createOrgFundingDateString(
+                      date,
+                    )}`}</CardSet>
+                    <CardSet icon={<MoneyIcon />}>
+                      {`Last Amount: $${numFormatter.format(raisedAmount)}M`}
+                    </CardSet>
+                  </div>
+                  {i !== fundingRounds.length - 1 && (
+                    <div className="flex h-fit flex-col justify-center">
+                      <hr className="border-t border-dashed border-white/20" />
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+
+        {investors.length > 0 && (
+          <>
+            <div className="flex h-fit flex-col justify-center">
+              <hr className="border-t border-white/10" />
+            </div>
+
+            <Heading size="sm" fw="semibold">
+              Investors
+            </Heading>
+
+            <div className="flex flex-wrap items-center gap-4 pl-1">
+              {investors.map(({ name }) => (
+                <CardSet key={name} icon={<MoneyIcon />}>
+                  {name}
+                </CardSet>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
