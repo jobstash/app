614c3e8af7451b6f05c0b3cffdfba2928f53a978
John Ballesteros <johnshift.dev@gmail.com>
2023-05-05 15:24:41 +0800

    chore: fix build errors

--- src/features/admin/pages/org-edit-project.tsx
+++ src/features/admin/pages/org-edit-project.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+
 import {
   Checkbox,
   Flex,
@@ -10,6 +12,7 @@ import {
 } from '@mantine/core';
 
 import { CHECK_WALLET_ROLES } from '~/features/auth/core/constants';
+import { slugify } from '~/shared/utils';
 
 import { HandWavingIcon } from '../components/icons/hand-waving-icon';
 import { MegaphoneIcon } from '../components/icons/megaphone-icon';
@@ -22,97 +25,110 @@ const breadCrumbs = [
   { title: 'Edit Project', href: '/godmode/organizations/projects/edit' },
 ];
 
-const OrgEditProjectPage = () => (
-  <AdminLayout
-    breadCrumbs={breadCrumbs}
-    sideNav={<OrgSideNavs activeLabel="Projects" />}
-  >
-    <Stack w="60%" spacing={30} pt={20}>
-      <Paper
-        p={30}
-        bg="rgba(255, 255, 255, 0.05)"
-        radius="md"
-        sx={(theme) => ({
-          border: `1px solid ${theme.colors.violet[9]}`,
-        })}
-      >
-        <Stack spacing={30}>
-          <Grid align="end">
-            <Grid.Col span={11}>
-              <Stack>
-                <Title c="violet">Hey!</Title>
-                <Title order={4}>
-                  Are you a new project that hasn&#39;t been deployed to mainnet
-                  yet?
-                </Title>
-              </Stack>
-            </Grid.Col>
-            <Grid.Col span={1} h="100%">
-              <Flex align="end" direction="column">
-                <HandWavingIcon />
-              </Flex>
-            </Grid.Col>
-          </Grid>
-          <Checkbox
-            label="Yup, project is still in the making!"
-            radius="md"
-            size="lg"
-            sx={{ cursor: 'pointer' }}
-          />
-        </Stack>
-      </Paper>
-      <Paper p={30} bg="rgba(255, 255, 255, 0.05)" radius="md">
-        <Stack spacing={20}>
-          <Grid align="start">
-            <Grid.Col span={11}>
-              <Flex gap="sm">
-                <Title order={2} c="white">
-                  Quick Setup with
-                </Title>
-                <Title order={2} c="violet">
-                  DefiLlama
-                </Title>
-              </Flex>
-            </Grid.Col>
-            <Grid.Col span={1} h="100%">
-              <Flex align="end" direction="column">
-                <MegaphoneIcon />
-              </Flex>
-            </Grid.Col>
-          </Grid>
-          <Stack spacing={15} w="90%">
-            <Text c="dimmed">
-              Pooling resources can be helpful here. We use data from DefiLiama
-              and Dune to ease up the input work as well as automate the inputs
-              from any possible future changes in the project.
-            </Text>
-            <Text c="dimmed">
-              DefiLiama can provide us with info such as: Project Name,
-              Description, URL, Category, and TVL. and make life easier for
-              everyone.
-            </Text>
-            <Text c="dimmed">
-              Paste the DefiLiama URL in the input field below, and make life
-              easier for everyone.
-            </Text>
+const OrgEditProjectPage = () => {
+  const { query } = useRouter();
+
+  const splitIndex = (query.key as string).lastIndexOf('-');
+  const orgName = (query.key as string).slice(0, splitIndex);
+  const orgId = (query.key as string).slice(splitIndex + 1);
+
+  return (
+    <AdminLayout
+      breadCrumbs={breadCrumbs}
+      sideNav={
+        <OrgSideNavs
+          keySegment={slugify(`${orgName} ${orgId}`)}
+          activeLabel="Projects"
+        />
+      }
+    >
+      <Stack w="60%" spacing={30} pt={20}>
+        <Paper
+          p={30}
+          bg="rgba(255, 255, 255, 0.05)"
+          radius="md"
+          sx={(theme) => ({
+            border: `1px solid ${theme.colors.violet[9]}`,
+          })}
+        >
+          <Stack spacing={30}>
+            <Grid align="end">
+              <Grid.Col span={11}>
+                <Stack>
+                  <Title c="violet">Hey!</Title>
+                  <Title order={4}>
+                    Are you a new project that hasn&#39;t been deployed to
+                    mainnet yet?
+                  </Title>
+                </Stack>
+              </Grid.Col>
+              <Grid.Col span={1} h="100%">
+                <Flex align="end" direction="column">
+                  <HandWavingIcon />
+                </Flex>
+              </Grid.Col>
+            </Grid>
+            <Checkbox
+              label="Yup, project is still in the making!"
+              radius="md"
+              size="lg"
+              sx={{ cursor: 'pointer' }}
+            />
           </Stack>
-          <Grid pt={20} align="center">
-            <Grid.Col span={2}>
-              <Title order={4}>DefiLlama URL</Title>
-            </Grid.Col>
-            <Grid.Col span={9}>
-              <TextInput
-                placeholder="Enter DefiLlama URL here ..."
-                size="lg"
-                radius="lg"
-              />
-            </Grid.Col>
-          </Grid>
-        </Stack>
-      </Paper>
-    </Stack>
-  </AdminLayout>
-);
+        </Paper>
+        <Paper p={30} bg="rgba(255, 255, 255, 0.05)" radius="md">
+          <Stack spacing={20}>
+            <Grid align="start">
+              <Grid.Col span={11}>
+                <Flex gap="sm">
+                  <Title order={2} c="white">
+                    Quick Setup with
+                  </Title>
+                  <Title order={2} c="violet">
+                    DefiLlama
+                  </Title>
+                </Flex>
+              </Grid.Col>
+              <Grid.Col span={1} h="100%">
+                <Flex align="end" direction="column">
+                  <MegaphoneIcon />
+                </Flex>
+              </Grid.Col>
+            </Grid>
+            <Stack spacing={15} w="90%">
+              <Text c="dimmed">
+                Pooling resources can be helpful here. We use data from
+                DefiLiama and Dune to ease up the input work as well as automate
+                the inputs from any possible future changes in the project.
+              </Text>
+              <Text c="dimmed">
+                DefiLiama can provide us with info such as: Project Name,
+                Description, URL, Category, and TVL. and make life easier for
+                everyone.
+              </Text>
+              <Text c="dimmed">
+                Paste the DefiLiama URL in the input field below, and make life
+                easier for everyone.
+              </Text>
+            </Stack>
+            <Grid pt={20} align="center">
+              <Grid.Col span={2}>
+                <Title order={4}>DefiLlama URL</Title>
+              </Grid.Col>
+              <Grid.Col span={9}>
+                <TextInput
+                  placeholder="Enter DefiLlama URL here ..."
+                  size="lg"
+                  radius="lg"
+                />
+              </Grid.Col>
+            </Grid>
+          </Stack>
+        </Paper>
+      </Stack>
+    </AdminLayout>
+  );
+};
 
 OrgEditProjectPage.requiredRole = CHECK_WALLET_ROLES.ADMIN;
 
--- src/features/admin/pages/org-jobs.tsx
+++ src/features/admin/pages/org-jobs.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+
 import {
   ActionIcon,
   Box,
@@ -17,6 +19,7 @@ import {
   MoneyIcon,
   UsersThreeIcon,
 } from '~/shared/components';
+import { slugify } from '~/shared/utils';
 
 import { CircleCheckIcon } from '../components/icons/circle-check-icon';
 import { JobDataSourceIcon } from '../components/icons/job-data-source-icon';
@@ -29,154 +32,169 @@ const breadCrumbs = [
   { title: 'My Jobs', href: '/godmode/organizations/jobs' },
 ];
 
-const OrgJobsPage = () => (
-  <AdminLayout
-    breadCrumbs={breadCrumbs}
-    sideNav={<OrgSideNavs activeLabel="Projects" />}
-  >
-    <Stack w="60%" spacing={15} pt={20}>
-      <Flex justify="end" gap="md" align="center">
-        <Text>Jobs: 4</Text>
-        <ActionIcon variant="subtle">
-          <RefreshIcon />
-        </ActionIcon>
-      </Flex>
-      <Paper p={40} bg="rgba(255, 255, 255, 0.05)" radius="xl">
-        <Stack spacing={15}>
-          <Flex justify="space-between" align="center">
-            <Title order={2} c="white">
-              Senior Frontend Developer
-            </Title>
-            <Text>Posted: 21/02/2022</Text>
-          </Flex>
+const OrgJobsPage = () => {
+  const { query } = useRouter();
 
-          <Flex gap="xs" align="center">
-            <CircleCheckIcon />
-            <Text>Status: Active</Text>
-            <Switch checked color="green" />
-          </Flex>
+  const splitIndex = (query.key as string).lastIndexOf('-');
+  const orgName = (query.key as string).slice(0, splitIndex);
+  const orgId = (query.key as string).slice(splitIndex + 1);
 
-          <Flex gap="xs" align="center">
-            <JobDataSourceIcon />
-            <Text>Source of Data: DefiLlama</Text>
-          </Flex>
+  return (
+    <AdminLayout
+      breadCrumbs={breadCrumbs}
+      sideNav={
+        <OrgSideNavs
+          keySegment={slugify(`${orgName} ${orgId}`)}
+          activeLabel="Projects"
+        />
+      }
+    >
+      <Stack w="60%" spacing={15} pt={20}>
+        <Flex justify="end" gap="md" align="center">
+          <Text>Jobs: 4</Text>
+          <ActionIcon variant="subtle">
+            <RefreshIcon />
+          </ActionIcon>
+        </Flex>
+        <Paper p={40} bg="rgba(255, 255, 255, 0.05)" radius="xl">
+          <Stack spacing={15}>
+            <Flex justify="space-between" align="center">
+              <Title order={2} c="white">
+                Senior Frontend Developer
+              </Title>
+              <Text>Posted: 21/02/2022</Text>
+            </Flex>
 
-          <Box py={5}>
-            <hr className="border-t border-white/10" />
-          </Box>
+            <Flex gap="xs" align="center">
+              <CircleCheckIcon />
+              <Text>Status: Active</Text>
+              <Switch checked color="green" />
+            </Flex>
 
-          <Flex gap="md" align="center">
-            <Button
-              left={<LevelIcon />}
-              className="cursor-default"
-              variant="subtle"
-            >
-              <Text size="md">Level: Senior</Text>
-            </Button>
-            <Button
-              left={<MoneyIcon />}
-              className="cursor-default"
-              variant="subtle"
-            >
-              <Text size="md">Salary: $60-90K/Year</Text>
-            </Button>
-            <Button
-              left={<LocationIcon />}
-              className="cursor-default"
-              variant="subtle"
-            >
-              <Text size="md">Remote</Text>
-            </Button>
-            <Button
-              left={<UsersThreeIcon />}
-              className="cursor-default"
-              variant="subtle"
-            >
-              <Text size="md">Team Size: 14</Text>
-            </Button>
-          </Flex>
+            <Flex gap="xs" align="center">
+              <JobDataSourceIcon />
+              <Text>Source of Data: DefiLlama</Text>
+            </Flex>
 
-          <Box py={5}>
-            <hr className="border-t border-white/10" />
-          </Box>
+            <Box py={5}>
+              <hr className="border-t border-white/10" />
+            </Box>
 
-          <Stack spacing={10}>
-            <Title order={4} c="white">
-              Role
-            </Title>
-            <Text c="dimmed" size="md">
-              As a Senior Frontend Engineer, you will help lead our team in
-              building well-designed user experiences that are unique,
-              trustworthy, and scalable across all frontend properties. You will
-              also be given the opportunity to influence the direction, design,
-              and execution of all future product experiences. We work in React,
-              TypeScript, Elixir, Elm, Solidity, Rust, and are regularly pulling
-              in functional programming languages and tools, forever learning
-              and optimizing everything we make in the name of correct coding.
-            </Text>
+            <Flex gap="md" align="center">
+              <Button
+                left={<LevelIcon />}
+                className="cursor-default"
+                variant="subtle"
+              >
+                <Text size="md">Level: Senior</Text>
+              </Button>
+              <Button
+                left={<MoneyIcon />}
+                className="cursor-default"
+                variant="subtle"
+              >
+                <Text size="md">Salary: $60-90K/Year</Text>
+              </Button>
+              <Button
+                left={<LocationIcon />}
+                className="cursor-default"
+                variant="subtle"
+              >
+                <Text size="md">Remote</Text>
+              </Button>
+              <Button
+                left={<UsersThreeIcon />}
+                className="cursor-default"
+                variant="subtle"
+              >
+                <Text size="md">Team Size: 14</Text>
+              </Button>
+            </Flex>
+
+            <Box py={5}>
+              <hr className="border-t border-white/10" />
+            </Box>
+
+            <Stack spacing={10}>
+              <Title order={4} c="white">
+                Role
+              </Title>
+              <Text c="dimmed" size="md">
+                As a Senior Frontend Engineer, you will help lead our team in
+                building well-designed user experiences that are unique,
+                trustworthy, and scalable across all frontend properties. You
+                will also be given the opportunity to influence the direction,
+                design, and execution of all future product experiences. We work
+                in React, TypeScript, Elixir, Elm, Solidity, Rust, and are
+                regularly pulling in functional programming languages and tools,
+                forever learning and optimizing everything we make in the name
+                of correct coding.
+              </Text>
+            </Stack>
+
+            <Box py={5}>
+              <hr className="border-t border-white/10" />
+            </Box>
+
+            <Stack spacing={10}>
+              <Title order={4} c="white">
+                Team
+              </Title>
+              <Text c="dimmed" size="md">
+                We are a team of passionate, creative, and hardworking
+                individuals who are dedicated to building the future of finance.
+                We are a diverse group of people from all walks of life, with
+                different backgrounds and experiences. We are united by our
+                shared mission to create a more open, accessible, and fair
+                financial system.
+              </Text>
+            </Stack>
+
+            <Box py={5}>
+              <hr className="border-t border-white/10" />
+            </Box>
+
+            <Stack spacing={10}>
+              <Title order={4} c="white">
+                Benefits
+              </Title>
+              <Text c="dimmed" size="md">
+                Stock options, 3+ weeks of vacation, paid parental leave, full
+                medical, dental, and vision insurance, company on-sites across
+                the world, remote available
+              </Text>
+            </Stack>
+
+            <Box py={5}>
+              <hr className="border-t border-white/10" />
+            </Box>
+
+            <Stack spacing={10}>
+              <Title order={4} c="white">
+                Interview Process
+              </Title>
+              <Text c="dimmed" size="md">
+                Once you&#39;ve applied, please be patient :) it may take us up
+                to 2-3 weeks to get back to you! Don&#39;t meet every single
+                requirement? Studies have shown that women and people of color
+                are less likely to apply to jobs unless they meet every single
+                qualification. We are dedicated to building a diverse, inclusive
+                and authentic workplace, so if you&#39;re excited about this
+                role but your past experience doesn&#39;t align perfectly with
+                every qualification in the job description, we encourage you to
+                apply anyways.
+              </Text>
+            </Stack>
+
+            <Box py={5}>
+              <hr className="border-t border-white/10" />
+            </Box>
           </Stack>
-
-          <Box py={5}>
-            <hr className="border-t border-white/10" />
-          </Box>
-
-          <Stack spacing={10}>
-            <Title order={4} c="white">
-              Team
-            </Title>
-            <Text c="dimmed" size="md">
-              We are a team of passionate, creative, and hardworking individuals
-              who are dedicated to building the future of finance. We are a
-              diverse group of people from all walks of life, with different
-              backgrounds and experiences. We are united by our shared mission
-              to create a more open, accessible, and fair financial system.
-            </Text>
-          </Stack>
-
-          <Box py={5}>
-            <hr className="border-t border-white/10" />
-          </Box>
-
-          <Stack spacing={10}>
-            <Title order={4} c="white">
-              Benefits
-            </Title>
-            <Text c="dimmed" size="md">
-              Stock options, 3+ weeks of vacation, paid parental leave, full
-              medical, dental, and vision insurance, company on-sites across the
-              world, remote available
-            </Text>
-          </Stack>
-
-          <Box py={5}>
-            <hr className="border-t border-white/10" />
-          </Box>
-
-          <Stack spacing={10}>
-            <Title order={4} c="white">
-              Interview Process
-            </Title>
-            <Text c="dimmed" size="md">
-              Once you&#39;ve applied, please be patient :) it may take us up to
-              2-3 weeks to get back to you! Don&#39;t meet every single
-              requirement? Studies have shown that women and people of color are
-              less likely to apply to jobs unless they meet every single
-              qualification. We are dedicated to building a diverse, inclusive
-              and authentic workplace, so if you&#39;re excited about this role
-              but your past experience doesn&#39;t align perfectly with every
-              qualification in the job description, we encourage you to apply
-              anyways.
-            </Text>
-          </Stack>
-
-          <Box py={5}>
-            <hr className="border-t border-white/10" />
-          </Box>
-        </Stack>
-      </Paper>
-    </Stack>
-  </AdminLayout>
-);
+        </Paper>
+      </Stack>
+    </AdminLayout>
+  );
+};
 
 OrgJobsPage.requiredRole = CHECK_WALLET_ROLES.ADMIN;
 
