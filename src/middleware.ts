f80c0d397dd9300ee59bc82e35d6e3aac53d4182
John Ballesteros <johnshift.dev@gmail.com>
2023-05-09 15:29:03 +0800

    refactor: whitelist one specific jobpost from http-auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,30 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const whiteListed = [
+  'uniswap-labs-senior-backend-engineer-1pN28S',
+  'JobStash.svg',
+  'favicon.ico',
+  '_next',
+  'Logo-01.svg',
+  'Logo-02.svg',
+];
+
+const getIsWhiteListed = (url: string) => {
+  for (const str of whiteListed) {
+    if (url.includes(str)) return true;
+  }
+
+  return false;
+};
+
 export function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization');
   const url = req.nextUrl;
+  const isWhiteListed = getIsWhiteListed(url.toString());
+
+  if (isWhiteListed) {
+    return NextResponse.next();
+  }
 
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1];
