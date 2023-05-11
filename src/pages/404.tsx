b547aaddaf6fc435d84d8a90be0687eebe7c8ac4
John Ballesteros <johnshift.dev@gmail.com>
2023-05-12 07:46:56 +0800

    refactor: add custom 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import { Text } from '~/shared/components';
+
+const Custom404 = () => (
+  <div className="flex h-screen w-full items-center justify-center">
+    <Text>Page Not Found</Text>
+  </div>
+);
+
+export default Custom404;
