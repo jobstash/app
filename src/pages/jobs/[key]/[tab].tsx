06711c1ae377128f1e6fd6733eff57c40b33dac1
duckdegen <98649644+duckdegen@users.noreply.github.com>
2023-05-16 15:05:19 +0300

    Main (#80)
    * rename casing for defillama stuff (#73)
    
    * refactor: use node instead of next bin
    
    * refactor: revert change on Dockerfile
    
    * fix(casing): update defillama casing
    
    ---------
    
    Co-authored-by: John Ballesteros <johnshift.dev@gmail.com>
    
    * refactor: release the kraken (#75)
    
    Co-authored-by: John Ballesteros <johnshift.dev@gmail.com>
    
    * Chore/merge dev recent commits (#79)
    
    * refactor: move error guard inside list
    
    * refactor: revert twitter image to use jobstash (twitter does not render using google favicon)
    
    * Feature/job 448 (#76)
    
    * feature(buttons): fix buttons when has icon and it's on multiline
    
    * feature(mobile menu): fix menu item on his own line
    
    ---------
    
    Co-authored-by: cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
    
    * fix: brand not following redirect settings in next.config
    
    * refactor: remove /jobs route in sitemap - redirects if crawler is desktop
    
    * refactor: update job-post interfaces to match data
    
    ---------
    
    Co-authored-by: cristiano <cristiano@welance.com>
    Co-authored-by: cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
    
    ---------
    
    Co-authored-by: John Ballesteros <johnshift.dev@gmail.com>
    Co-authored-by: John Ballesteros <89079900+johnshift@users.noreply.github.com>
    Co-authored-by: cristiano <cristiano@welance.com>
    Co-authored-by: cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>

--- src/pages/jobs/[key]/[tab].tsx
+++ src/pages/jobs/[key]/[tab].tsx
@@ -197,7 +197,7 @@ const JobsPage = ({ data: { initJob, fromSSR } }: Props) => {
 
   return (
     <>
-      {jobPost?.jobpost.shortUUID === '1pN28S' && (
+      {jobPost && (
         <MetaData
           title={titleMetaData}
           description={
