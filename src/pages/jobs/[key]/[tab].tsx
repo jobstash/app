a4a3326628d6fbd58934275d457e7203bb0f594a
DuckDegen <duckdegen@gmail.com>
2023-05-29 22:31:08 +0200

    fix(indexing): make all jobs indexeable

--- src/pages/jobs/[key]/[tab].tsx
+++ src/pages/jobs/[key]/[tab].tsx
@@ -197,7 +197,7 @@ const JobsPage = ({ data: { initJob, fromSSR } }: Props) => {
 
   return (
     <>
-      {jobPost?.jobpost.shortUUID === 'cJ1mvE' && (
+      {jobPost && (
         <MetaData
           title={titleMetaData}
           description={
