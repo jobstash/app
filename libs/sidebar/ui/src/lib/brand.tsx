c9ae493ca16bfb848b9081983e66682702dabdcb
John Ballesteros <johnshift.dev@gmail.com>
2023-06-21 16:56:09 +0800

    refactor(sidebar-ui): logo now redirects to landing page

--- libs/sidebar/ui/src/lib/brand.tsx
+++ libs/sidebar/ui/src/lib/brand.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { memo } from 'react';
 
 const Brand = () => (
-  <Link scroll={false} href="/jobs" className="lg:p-4 lg:pl-1">
+  <Link scroll={false} href="https://jobstash.xyz" className="lg:p-4 lg:pl-1">
     <Image
       priority
       src="/JobStash.svg"
