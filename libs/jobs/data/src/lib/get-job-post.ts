1ac30ac70d46ebadd0478a2beaebd9385b96850f
John Ballesteros <johnshift.dev@gmail.com>
2023-06-22 11:22:01 +0800

    chore: cleanup schema comments

--- libs/jobs/data/src/lib/get-job-post.ts
+++ libs/jobs/data/src/lib/get-job-post.ts
@@ -12,36 +12,3 @@ export const getJobPost = async (shortUuid: string) => {
 
   return mwFetch<JobPost>(url, options);
 };
-
-//
-// import type { JobPost } from '@jobstash/jobs/core';
-// import {
-//   SENTRY_MW_INVALID_JSON_RESPONSE,
-//   SENTRY_MW_NON_200_RESPONSE,
-// } from '@jobstash/shared/core';
-// import { getMwUrl, sentryMessage } from '@jobstash/shared/utils';
-
-// const SENTRY_LABEL = `getJobPost`;
-
-// export const getJobPost = async (shortUuid: string): Promise<JobPost> => {
-//   const mwUrl = getMwUrl();
-
-//   const res = await fetch(`${mwUrl}/jobs/details/${shortUuid}`);
-//   if (!res.ok) {
-//     sentryMessage(
-//       `${SENTRY_LABEL}: ${SENTRY_MW_NON_200_RESPONSE}`,
-//       `status = ${res.status}`,
-//     );
-//     throw new Error('Something went wrong :(');
-//   }
-
-//   let job: JobPost;
-//   try {
-//     job = await res.json();
-//   } catch {
-//     sentryMessage(SENTRY_LABEL, SENTRY_MW_INVALID_JSON_RESPONSE);
-//     throw new Error('Something went wrong :(');
-//   }
-
-//   return job;
-// };
--- libs/shared/core/src/lib/types.ts
+++ libs/shared/core/src/lib/types.ts
@@ -25,62 +25,18 @@ export interface TagElement {
 
 export type Undefined = Infer<typeof undefinedSchema>;
 
-//
-// export interface Technology {
-//   id: string;
-//   name: string;
-//   normalizedName: string;
-// }
 export type Technology = Infer<typeof technologySchema>;
 
-//
-// export interface Investor {
-//   name: string;
-//   id: string;
-// }
 export type Investor = Infer<typeof investorSchema>;
 
-//
-// export interface FundingRound {
-//   id: string;
-//   date: number;
-//   createdTimestamp: number;
-//   raisedAmount?: number;
-//   roundName?: string;
-//   sourceLink?: string;
-//   investors: Investor[];
-// }
 export type FundingRound = Infer<typeof fundingRoundSchema>;
 
-//
-// export interface Category {
-//   id: string;
-//   name: string;
-// }
 export type Category = Infer<typeof categorySchema>;
 
-//
-// export interface Chain {
-//   id: string;
-//   name: string;
-// }
 export type Chain = Infer<typeof chainSchema>;
 
-//
-// export interface Hack {
-//   id: string;
-//   date: number;
-//   classification: string;
-//   fundsLost: number;
-//   link: string;
-// }
 export type Hack = Infer<typeof hackSchema>;
 
-//
-// export interface Audit {
-//   auditor?: string;
-//   link: string;
-// }
 export type Audit = Infer<typeof auditSchema>;
 
 export type MwResponseFields = Infer<typeof mwResponseFieldsSchema>;
