f8c9b6ba1e0b31468bb7536797207d646e2e71f2
John Ballesteros <johnshift.dev@gmail.com>
2023-06-22 11:25:29 +0800

    refactor(organizations-core): update org schema to match mw

--- libs/organizations/core/src/lib/schemas.ts
+++ libs/organizations/core/src/lib/schemas.ts
@@ -1,26 +1,38 @@
 import myzod from 'myzod';
 
-import { projectSchema } from '@jobstash/projects/core';
-import { fundingRoundSchema, investorSchema } from '@jobstash/shared/core';
-
 export const organizationSchema = myzod.object(
   {
     id: myzod.string().min(1),
+    url: myzod.string().min(1),
     name: myzod.string().min(1),
-    description: myzod.string().min(1),
+    orgId: myzod.string().min(1),
     summary: myzod.string().min(1),
     location: myzod.string().min(1),
-    url: myzod.string().min(1),
-    github: myzod.string().min(1).nullable(),
-    twitter: myzod.string().min(1).nullable(),
-    discord: myzod.string().min(1).nullable(),
-    telegram: myzod.string().min(1).nullable(),
-    docs: myzod.string().min(1).nullable(),
+    description: myzod.string().min(1),
     jobsiteLink: myzod.string().min(1).nullable(),
-    fundingRounds: myzod.array(fundingRoundSchema),
-    investors: myzod.array(investorSchema),
+    docs: myzod.string().min(1).nullable(),
+    github: myzod.string().min(1).nullable(),
+    altName: myzod.string().min(1).nullable(),
+    discord: myzod.string().min(1).nullable(),
+    twitter: myzod.string().min(1).nullable(),
+    telegram: myzod.string().min(1).nullable(),
     headCount: myzod.number().min(1).nullable(),
-    projects: myzod.array(projectSchema),
+    createdTimestamp: myzod.number().nullable(),
+    updatedTimestamp: myzod.number().nullable(),
   },
   { allowUnknown: true },
 );
+
+export const orgPostSchema = myzod.intersection(
+  organizationSchema,
+  myzod.object({
+    // Add more fields here e.g. job count, project count (to be implemented in mw)
+  }),
+);
+
+export const orgListQueryPageSchema = myzod.object({
+  page: myzod.number(),
+  count: myzod.number(),
+  total: myzod.number(),
+  data: myzod.array(organizationSchema),
+});
