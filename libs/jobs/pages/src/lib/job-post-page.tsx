99537441893f7d28478643cf960b64dbce27e7b8
John Ballesteros <johnshift.dev@gmail.com>
2023-06-29 17:44:40 +0800

    fix: hide scrollbar for details panel

--- libs/jobs/pages/src/lib/job-post-page.tsx
+++ libs/jobs/pages/src/lib/job-post-page.tsx
@@ -128,7 +128,7 @@ export const JobPostPage = ({ initJob, fromSSR }: JobPostPageProps) => {
 
         <div
           className={cn(
-            'lg:hide-scrollbar fixed inset-0 h-screen overflow-y-auto bg-dark p-4 pt-6 transition-all lg:inset-auto lg:right-0 lg:top-0 lg:w-5/12 lg:px-6 lg:py-8 lg:pr-10',
+            'hide-scrollbar fixed inset-0 h-screen overflow-y-auto bg-dark p-4 pt-6 transition-all lg:inset-auto lg:right-0 lg:top-0 lg:w-5/12 lg:px-6 lg:py-8 lg:pr-10',
             { active: activeJob === initJob },
             { 'z-50': !showFilters },
             { '-z-50': showFilters },
