bc5fb76d55d77cd069672b427a89715d0b15f213
John Ballesteros <johnshift.dev@gmail.com>
2023-08-24 11:42:21 +0800

    refactor: temporarily removed top-banner

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -8,21 +8,14 @@ import Script from 'next/script';
 import { useEffect } from 'react';
 
 import { LoadingPage } from '@jobstash/shared/pages';
-import { useAtomValue } from 'jotai';
 import NProgress from 'nprogress';
 
 import { ANALYTICS_ID } from '@jobstash/shared/core';
-import { cn } from '@jobstash/shared/utils';
 
 import { AuthProvider } from '@jobstash/auth/state';
-import {
-  isOpenTopBannerAtom,
-  MantineProvider,
-  ReactQueryProvider,
-} from '@jobstash/shared/state';
+import { MantineProvider, ReactQueryProvider } from '@jobstash/shared/state';
 import { useNProgress } from '@jobstash/shared/state';
 
-import { TopBanner } from '@jobstash/shared/ui';
 import { WagmiSiweSync } from '@jobstash/auth/feature';
 
 NProgress.configure({
@@ -60,8 +53,6 @@ const App = ({ Component, pageProps }: AppProps) => {
     };
   }, [router, startNProgress, stopNProgress]);
 
-  const isOpenTopBanner = useAtomValue(isOpenTopBannerAtom);
-
   return (
     <>
       <Head>
@@ -94,10 +85,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       <ReactQueryProvider dehydratedState={pageProps.dehydratedState}>
         <MantineProvider>
           <AuthProvider screenLoader={<LoadingPage />}>
-            <TopBanner />
-            <div className={cn({ 'pt-10': isOpenTopBanner })}>
-              <Component {...pageProps} />
-            </div>
+            <Component {...pageProps} />
             <WagmiSiweSync />
           </AuthProvider>
         </MantineProvider>
