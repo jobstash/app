f1b5c57fa46a352edb6e30f9a051fbdf51ec29b7
John Ballesteros <johnshift.dev@gmail.com>
2023-09-20 18:58:41 +0800

    fix(right-panel-ui): project logo sometimes missing

--- libs/right-panel/ui/src/lib/right-panel-project-card-header.tsx
+++ libs/right-panel/ui/src/lib/right-panel-project-card-header.tsx
@@ -1,7 +1,6 @@
 import { memo } from 'react';
 
 import { type TagElement } from '@jobstash/shared/core';
-import { getLogoUrl } from '@jobstash/shared/utils';
 
 import RightPanelProjectCardTitle from '../right-panel-project-card-title';
 
@@ -10,17 +9,13 @@ import RightPanelProjectCardSocials from './right-panel-project-card-socials';
 interface Props {
   name: string;
   url: string;
-  logo?: string;
+  logo: string | null;
   socials: TagElement[];
 }
 
 const RightPanelProjectCardHeader = ({ name, url, logo, socials }: Props) => (
   <div className="flex flex-col gap-5">
-    <RightPanelProjectCardTitle
-      name={name}
-      url={url}
-      logo={getLogoUrl(url, logo)}
-    />
+    <RightPanelProjectCardTitle name={name} url={url} logo={logo} />
     <RightPanelProjectCardSocials socials={socials} />
   </div>
 );
