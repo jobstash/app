944771fdfc4a0e74204f17ef0aaa01d7fcdb63ed
John Ballesteros <johnshift.dev@gmail.com>
2023-10-03 18:41:24 +0800

    refactor(home-ui): add hover styles on keywords

--- libs/home/ui/src/lib/home-search/home-search-popular-keywords.tsx
+++ libs/home/ui/src/lib/home-search/home-search-popular-keywords.tsx
@@ -38,13 +38,13 @@ const HomeSearchPopularKeywords = () => (
     {keywords.map((w, index) => (
       <motion.div
         key={w}
-        className="border border-white/20 p-2 rounded-xl"
+        className="border border-white/20 transition-all duration-300 p-2 rounded-xl cursor-pointer hover:bg-darker-gray hover:border-white/40"
         variants={keywordAnimationVariants}
         initial="initial"
         animate="animate"
         custom={index}
       >
-        <Text color="dimmed">{w}</Text>
+        <Text className="text-white/80 hover:text-white">{w}</Text>
       </motion.div>
     ))}
   </div>
