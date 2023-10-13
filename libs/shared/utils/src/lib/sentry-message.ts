f5267c7fda793c4053cac983b4a09f22a8310769
John Ballesteros <johnshift.dev@gmail.com>
2023-10-13 14:07:17 +0800

    refactor: add is-debug to next public envs

--- libs/shared/utils/src/lib/sentry-message.ts
+++ libs/shared/utils/src/lib/sentry-message.ts
@@ -1,13 +1,15 @@
 import * as Sentry from '@sentry/nextjs';
 import { type SeverityLevel } from '@sentry/nextjs';
 
+import { IS_DEBUG } from '@jobstash/shared/core';
+
 export const sentryMessage = (
   label: string,
   msg: string,
   level?: SeverityLevel,
 ) => {
   const message = `${label}: ${msg}`;
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production' && IS_DEBUG !== 'true') {
     Sentry.captureMessage(message, level ?? 'debug');
   } else {
     console.log(message);
