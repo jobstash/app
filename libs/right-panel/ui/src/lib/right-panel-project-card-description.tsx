e2ee8b47e49dda1274c9e82797872724d339567f
John Ballesteros <johnshift.dev@gmail.com>
2023-10-31 18:43:38 +0800

    refactor: adjust details-panel cards spacing

--- libs/right-panel/ui/src/lib/right-panel-project-card-description.tsx
+++ libs/right-panel/ui/src/lib/right-panel-project-card-description.tsx
@@ -11,17 +11,15 @@ const RightPanelProjectCardDescription = ({ description }: Props) => {
 
   return (
     <>
-      <div className="flex h-4 flex-col justify-center">
+      <div className="flex h-2 flex-col justify-center">
         <hr className="border-t border-white/10" />
       </div>
 
-      <div className="flex flex-col gap-5">
-        <div className="flex flex-col gap-2">
-          <Heading size="sm" fw="semibold">
-            Description
-          </Heading>
-          <Text color="dimmed">{description}</Text>
-        </div>
+      <div className="flex flex-col gap-2">
+        <Heading size="sm" fw="semibold">
+          Description
+        </Heading>
+        <Text color="dimmed">{description}</Text>
       </div>
     </>
   );
