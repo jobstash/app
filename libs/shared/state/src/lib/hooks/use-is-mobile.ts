066941de1b08d851d241e518acfe4f37e03680fb
John Ballesteros <johnshift.dev@gmail.com>
2023-11-16 20:24:02 +0800

    refactor: update donate-modal text (shorter)

--- libs/shared/state/src/lib/hooks/use-is-mobile.ts
+++ libs/shared/state/src/lib/hooks/use-is-mobile.ts
@@ -2,4 +2,5 @@ import { MEDIA_QUERY_MOBILE } from '@jobstash/shared/core';
 
 import { useMediaQuery } from './use-media-query';
 
-export const useIsMobile = () => useMediaQuery(MEDIA_QUERY_MOBILE, true);
+export const useIsMobile = (mediaQuery?: string) =>
+  useMediaQuery(mediaQuery ?? MEDIA_QUERY_MOBILE, true);
