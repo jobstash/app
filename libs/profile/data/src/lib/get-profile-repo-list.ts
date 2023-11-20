f9b08ba970f7851897d5ead6b27fc14794011f74
John Ballesteros <johnshift.dev@gmail.com>
2023-11-20 17:22:34 +0800

    refactor: use mw endpoint for profile-repo

--- libs/profile/data/src/lib/get-profile-repo-list.ts
+++ libs/profile/data/src/lib/get-profile-repo-list.ts
@@ -2,7 +2,7 @@ import {
   type ProfileRepoListQueryPage,
   profileRepoListQueryPageSchema,
 } from '@jobstash/profile/core';
-import { PAGE_SIZE } from '@jobstash/shared/core';
+import { MW_URL, PAGE_SIZE } from '@jobstash/shared/core';
 
 import { mwFetch } from '@jobstash/shared/data';
 
@@ -10,8 +10,8 @@ export const getProfileRepoList = async (
   page: number,
 ): Promise<ProfileRepoListQueryPage> => {
   //
-  // const url = `${MW_URL}/profile/repositories?page=${page.toSTring()}&limit=${PAGE_SIZE}&wallet=${wallet}`;
-  const url = `/api/fakers/profile/repositories?page=${page.toString()}&limit=${PAGE_SIZE}`;
+  // const url = `/api/fakers/profile/repositories?page=${page.toString()}&limit=${PAGE_SIZE}`;
+  const url = `${MW_URL}/profile/repositories?page=${page.toString()}&limit=${PAGE_SIZE}`;
 
   const options = {
     responseSchema: profileRepoListQueryPageSchema,
