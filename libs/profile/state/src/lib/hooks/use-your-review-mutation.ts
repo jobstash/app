218fbc423592817cd404cf59de65d9089c0f80ca
John Ballesteros <johnshift.dev@gmail.com>
2023-12-06 18:24:12 +0800

    fix: org-review forms reverting to previous values after submit

--- libs/profile/state/src/lib/hooks/use-your-review-mutation.ts
+++ libs/profile/state/src/lib/hooks/use-your-review-mutation.ts
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useAtom } from 'jotai';
 import { useAccount } from 'wagmi';
 
 import { type ProfileOrgReviewPayload } from '@jobstash/profile/core';
@@ -6,6 +7,7 @@ import { notifError, notifSuccess } from '@jobstash/shared/utils';
 
 import { postProfileOrgReview } from '@jobstash/profile/data';
 
+import { activeProfileOrgReviewAtom } from '../atoms/active-profile-org-review-atom';
 import { useProfileReviewsPageContext } from '../contexts/profile-reviews-page-context';
 
 export const useYourReviewMutation = () => {
@@ -13,13 +15,17 @@ export const useYourReviewMutation = () => {
   const { address } = useAccount();
   const queryClient = useQueryClient();
 
+  const [activeProfileOrgReview, setActiveProfileOrgReview] = useAtom(
+    activeProfileOrgReviewAtom,
+  );
+
   const { isLoading, mutate } = useMutation({
     mutationFn: (payload: ProfileOrgReviewPayload) =>
       postProfileOrgReview(payload),
     onMutate() {
       setIsLoadingCard(true);
     },
-    onSuccess({ message }) {
+    onSuccess({ message }, payload) {
       // TODO: Update org-review-list,
       // TODO: Update org-review state (should update disableSave flag)
       notifSuccess({ title: 'Review Updated', message });
@@ -28,6 +34,14 @@ export const useYourReviewMutation = () => {
       queryClient.invalidateQueries({
         queryKey: ['profile-org-review-list', address],
       });
+
+      if (activeProfileOrgReview) {
+        const review = (({ orgId, ...props }) => props)(payload);
+        setActiveProfileOrgReview({
+          ...activeProfileOrgReview,
+          review,
+        });
+      }
     },
     onError(error) {
       notifError({
