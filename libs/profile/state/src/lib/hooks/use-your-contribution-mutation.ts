ed4bd86e05e110c22aeddf80be24e6c20186624e
John Ballesteros <johnshift.dev@gmail.com>
2023-12-06 19:37:14 +0800

    fix: profile-repo forms reverting to  previous value after submit

--- libs/profile/state/src/lib/hooks/use-your-contribution-mutation.ts
+++ libs/profile/state/src/lib/hooks/use-your-contribution-mutation.ts
@@ -1,15 +1,21 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useAtom } from 'jotai';
 
 import { type ProfileRepoContributionPayload } from '@jobstash/profile/core';
 import { notifError, notifSuccess } from '@jobstash/shared/utils';
 
 import { postProfileRepoContribution } from '@jobstash/profile/data';
 
+import { activeProfileRepoAtom } from '../atoms/active-profile-repo-atom';
 import { useProfileRepoPageContext } from '../contexts/profile-repo-page-context';
 
 export const useYourContributionMutation = () => {
   const { setIsLoadingCard } = useProfileRepoPageContext();
   const queryClient = useQueryClient();
+
+  const [activeProfileRepo, setActiveProfileRepo] = useAtom(
+    activeProfileRepoAtom,
+  );
 
   const { isLoading, mutate } = useMutation({
     mutationFn: (payload: ProfileRepoContributionPayload) =>
@@ -18,7 +24,7 @@ export const useYourContributionMutation = () => {
     onMutate() {
       setIsLoadingCard(true);
     },
-    onSuccess({ message }) {
+    onSuccess({ message }, payload) {
       // TODO: Update profile-repo-list
       // TODO: Update profile repo state (should update disableSave flag)
       notifSuccess({
@@ -28,6 +34,16 @@ export const useYourContributionMutation = () => {
 
       // Invalidate profile-repo-list
       queryClient.invalidateQueries({ queryKey: ['profile-repo-list'] });
+
+      if (activeProfileRepo) {
+        setActiveProfileRepo({
+          ...activeProfileRepo,
+          contribution: {
+            ...activeProfileRepo.contribution,
+            summary: payload.contribution,
+          },
+        });
+      }
     },
     onError(error) {
       notifError({
