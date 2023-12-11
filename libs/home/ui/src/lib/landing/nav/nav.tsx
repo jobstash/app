f8586e07f71167d1bd583b2c8bc144af69bba1c5
John Ballesteros <johnshift.dev@gmail.com>
2023-12-11 17:18:10 +0800

    refactor: update homepage app-button link

--- libs/home/ui/src/lib/landing/nav/nav.tsx
+++ libs/home/ui/src/lib/landing/nav/nav.tsx
@@ -45,7 +45,7 @@ const LandingNav = () => {
         <LinkButton
           variant="primary"
           linkProps={{
-            href: FRONTEND_URL,
+            href: `${FRONTEND_URL}/jobs`,
           }}
         >
           <Text fw="semibold" className="px-2">
