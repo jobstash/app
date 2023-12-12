ecfb4a956b843e016782fea860eb71d4c5fa0824
John Ballesteros <johnshift.dev@gmail.com>
2023-12-12 20:49:03 +0800

    refactor: update sentry config to not run in staging

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -1,6 +1,9 @@
 import * as Sentry from '@sentry/nextjs';
 
-if (process.env.NODE_ENV === 'production') {
+if (
+  process.env.NODE_ENV === 'production' &&
+  process.env.NEXT_PUBLIC_IS_DEBUG !== 'true'
+) {
   Sentry.init({
     dsn: 'https://714cca1095c042fda9f542dde5ed7063@o4504495959703552.ingest.sentry.io/4504519276363776',
     tracesSampleRate: 1,
