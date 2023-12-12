36899d69c64f39049a90e8be19f9ade5e47243f4
John Ballesteros <johnshift.dev@gmail.com>
2023-12-12 21:17:08 +0800

    chore: revert staletime for check-wallet query

--- libs/auth/state/src/lib/hooks/use-check-wallet.ts
+++ libs/auth/state/src/lib/hooks/use-check-wallet.ts
@@ -11,6 +11,7 @@ export const useCheckWallet = () => {
     queryFn: () => getCheckWallet(),
     select: (data) => data.data,
     enabled: isConnected, // Only fetch when wallet is connected
+    staleTime: 1000 * 60 * 60,
   });
 
   return {
