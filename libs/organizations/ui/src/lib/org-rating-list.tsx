e90f044d9f656985fee9e3d1da658b4793772691
John Ballesteros <johnshift.dev@gmail.com>
2024-01-08 14:14:08 +0800

    refactor: only show ratings with value

--- libs/organizations/ui/src/lib/org-rating-list.tsx
+++ libs/organizations/ui/src/lib/org-rating-list.tsx
@@ -5,35 +5,35 @@ import {
   type OrgRating,
 } from '@jobstash/organizations/core';
 
-import { EmptyStarIcon, Text } from '@jobstash/shared/ui';
+import { Text } from '@jobstash/shared/ui';
 
 interface Props {
   orgRating: OrgRating;
 }
 
-const OrgRatingList = ({ orgRating }: Props) => (
-  <div className="flex flex-wrap gap-x-12 gap-y-4">
-    {Object.entries(orgRating).map(([label, rating]) => (
-      <div key={label} className="flex items-center gap-2">
-        <Text fw="bold">
-          {rating && rating > 0 ? rating.toFixed(1) : 'n/d'}
-        </Text>
-        {rating ? (
+const OrgRatingList = ({ orgRating }: Props) => {
+  const ratings = Object.entries(orgRating).filter(
+    ([, rating]) => rating !== null,
+  ) as [string, number][];
+
+  return (
+    <div className="flex flex-wrap gap-x-12 gap-y-4">
+      {ratings.map(([label, rating]) => (
+        <div key={label} className="flex items-center gap-2">
+          <Text fw="bold">{rating.toFixed(1)}</Text>
           <Rating
             count={1}
             fractions={5}
             value={(rating ?? 0) / 5}
             color="gold"
           />
-        ) : (
-          <EmptyStarIcon />
-        )}
-        <Text>
-          {ORG_RATING_LABELS[label as keyof typeof ORG_RATING_LABELS]}
-        </Text>
-      </div>
-    ))}
-  </div>
-);
+          <Text>
+            {ORG_RATING_LABELS[label as keyof typeof ORG_RATING_LABELS]}
+          </Text>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default OrgRatingList;
