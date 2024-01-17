02a421f995c3232487445e2101db9cabac49e6e4
John Ballesteros <johnshift.dev@gmail.com>
2024-01-17 11:20:30 +0800

    refactor: responsive got-it card layout

--- libs/profile/ui/src/lib/got-it-card.tsx
+++ libs/profile/ui/src/lib/got-it-card.tsx
@@ -1,6 +1,8 @@
 import { memo, ReactNode } from 'react';
 
-import { Button, Heading, Hexagon } from '@jobstash/shared/ui';
+import { cn } from '@jobstash/shared/utils';
+
+import { Heading, Hexagon, Text } from '@jobstash/shared/ui';
 
 interface Props {
   icon: ReactNode;
@@ -13,26 +15,29 @@ const GotItCard = (props: Props) => {
   const { icon, title, onClick, children } = props;
 
   return (
-    <div className="flex border border-white/10 rounded-3xl pt-4 pr-12 bg-dark">
-      <div className="flex radius-xl gap-1">
-        <div>
+    <div className="flex flex-col gap-12">
+      <div className="flex border border-white/10 rounded-3xl p-6 md:p-0 md:pr-8 bg-dark">
+        <div className="hidden md:flex md:-ml-4 items-center justify-center">
           <Hexagon icon={icon} />
         </div>
-        <div className="flex flex-col py-6 space-y-4">
-          <Heading fw="semibold" size="xl">
-            {title}
-          </Heading>
+        <div className="flex flex-col gap-4 md:gap-4 md:py-8">
+          <Heading className="text-xl md:text-2xl">{title}</Heading>
+
           {children}
 
           {onClick && (
-            <div className="flex justify-end">
-              <Button
-                variant="primary"
-                className="px-10 py-2.5"
+            <div className="flex w-full md:justify-end pt-2">
+              <button
+                type="button"
+                className={cn(
+                  'flex w-full items-center justify-center rounded-lg bg-gradient-to-l from-primary to-tertiary py-3 transition duration-150 gap-1 ease-in-out md:max-w-[120px] ',
+                )}
                 onClick={onClick}
               >
-                OK Got it!
-              </Button>
+                <Text size="sm" fw="bold">
+                  OK Got It!
+                </Text>
+              </button>
             </div>
           )}
         </div>
