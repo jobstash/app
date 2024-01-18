db0e3ab6b2eaf9e7666b61bd328c896f7e19c9ab
John Ballesteros <johnshift.dev@gmail.com>
2024-01-18 11:53:23 +0800

    refactor: responsive ratings form

--- libs/profile/ui/src/lib/ratings/ratings-item.tsx
+++ libs/profile/ui/src/lib/ratings/ratings-item.tsx
@@ -3,6 +3,7 @@ import { Rating } from '@mantine/core';
 import { type OrgRating } from '@jobstash/organizations/core';
 
 import { useProfileOrgReviewFormContext } from '@jobstash/profile/state';
+import { useIsMobile } from '@jobstash/shared/state';
 
 import { Text } from '@jobstash/shared/ui';
 
@@ -14,14 +15,16 @@ const RatingItem = ({ ratingKey }: Props) => {
   const { rating, setRating, getRatingTitle } =
     useProfileOrgReviewFormContext();
 
+  const isXS = useIsMobile('(max-width: 374px)');
+
   return (
     <div className="flex gap-8 items-center pl-[5%]">
-      <div className="flex justify-end w-[38%]">
-        <Text size="lg">{getRatingTitle(ratingKey)}</Text>
+      <div className="flex md:justify-end w-[38%]">
+        <Text className="md:text-lg">{getRatingTitle(ratingKey)}</Text>
       </div>
-      <div className="w-[55%]">
+      <div className="w-[55%] flex justify-end sm:justify-start">
         <Rating
-          size="lg"
+          size={isXS ? 'md' : 'lg'}
           value={rating[ratingKey] ?? 0}
           color="gold"
           onChange={(v) => setRating((prev) => ({ ...prev, [ratingKey]: v }))}
