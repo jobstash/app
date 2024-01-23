1495e5088d47b3f3cc79dd3eaa53a1335a60b83c
John Ballesteros <johnshift.dev@gmail.com>
2024-01-23 12:17:21 +0800

    refactor: draggable right-panel tabs

--- libs/right-panel/ui/src/lib/right-panel-tabs.tsx
+++ libs/right-panel/ui/src/lib/right-panel-tabs.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 
 import { type RightPanelTab } from '@jobstash/right-panel/core';
 
-import { Spinner } from '@jobstash/shared/ui';
+import { DraggableWrapper, Spinner } from '@jobstash/shared/ui';
 
 import RightPanelTabButton from './right-panel-tab-button';
 
@@ -13,18 +13,21 @@ interface Props {
 }
 
 const RightPanelTabs = ({ isLoading, currentTab, tabs }: Props) => (
-  <div className="flex items-center flex-wrap gap-4 border-t border-white/10 pt-8">
-    {tabs.map(({ text, tabSegment, href }) => (
-      <RightPanelTabButton
-        key={text}
-        text={text}
-        currentTab={currentTab}
-        tabSegment={tabSegment}
-        href={href}
-      />
-    ))}
-    {isLoading && <Spinner />}
-  </div>
+  <>
+    <hr className="border-t border-white/10 -my-2" />
+    <DraggableWrapper className="flex items-center gap-4 py-1">
+      {tabs.map(({ text, tabSegment, href }) => (
+        <RightPanelTabButton
+          key={text}
+          text={text}
+          currentTab={currentTab}
+          tabSegment={tabSegment}
+          href={href}
+        />
+      ))}
+      {isLoading && <Spinner />}
+    </DraggableWrapper>
+  </>
 );
 
 export default memo(RightPanelTabs);
