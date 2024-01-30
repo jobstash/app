6886b007f3025c45f8b9f46d83cc0e15d61a842a
John Ballesteros <johnshift.dev@gmail.com>
2024-01-30 12:49:59 +0800

    refactor: remove max-scale in viewport

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -44,7 +44,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
         />
       </Head>
       {ANALYTICS_ID && (
