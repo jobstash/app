0c5b45bbaa18ddcdbb6be5ad06995e9bacec20e1
John Ballesteros <johnshift.dev@gmail.com>
2024-01-30 13:45:31 +0800

    refactor: remove user-scaleable in viewport meta

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -42,10 +42,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       {ANALYTICS_ID && (
         <>
