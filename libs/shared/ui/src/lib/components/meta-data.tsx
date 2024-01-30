46867dcb7c89e45643301c00ee94c4846496b25a
John Ballesteros <johnshift.dev@gmail.com>
2024-01-30 15:53:37 +0800

    refactor: restore previous meta-tags

--- libs/shared/ui/src/lib/components/meta-data.tsx
+++ libs/shared/ui/src/lib/components/meta-data.tsx
@@ -1,10 +1,7 @@
 import Head from 'next/head';
 import { memo } from 'react';
 
-import { JOB_FRAME_URL } from '@jobstash/shared/core';
-
 interface Props {
-  id: string;
   title: string;
   description: string;
   url: string;
@@ -29,7 +26,6 @@ interface Props {
 }
 
 const MetaData = ({
-  id,
   title,
   description,
   url,
@@ -43,60 +39,35 @@ const MetaData = ({
     <title>{title}</title>
     <meta name="title" content={title} />
     <meta name="description" content={description} />
-    {/* <link rel="canonical" href={url} /> */}
-
-    {/** Farcaster Frames */}
-    <meta name="fc:frame" content="vNext" />
-    <meta name="fc:frame:button:1" content="Prev" />
-    <meta name="fc:frame:button:2" content="Next" />
-    <meta
-      name="fc:frame:image"
-      content={`${JOB_FRAME_URL}/api/jobs?id=${id}&tab=details`}
-    />
-    <meta
-      name="fc:frame:post_url"
-      content={`${JOB_FRAME_URL}/api/frame/jobs/${id}/details`}
-    />
+    <link rel="canonical" href={url} />
 
     {/* Open Graph */}
-    {/* <meta property="og:type" content="website" /> */}
-    {/* <meta property="og:url" content={og?.url ?? url} /> */}
-    {/* <meta property="og:title" content={og?.title ?? title} /> */}
-    <meta property="og:title" content={title} />
-    {/* <meta property="og:description" content={og?.description ?? description} /> */}
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={og?.url ?? url} />
+    <meta property="og:title" content={og?.title ?? title} />
+    <meta property="og:description" content={og?.description ?? description} />
     <meta property="og:description" content={description} />
-    {/* <meta property="og:image" content={og?.image ?? image} /> */}
-    <meta
-      property="og:image"
-      content={`${JOB_FRAME_URL}/api/jobs?id=${id}&tab=details`}
-    />
-    {/* <meta property="og:image:width" content="1200" /> */}
-    {/* <meta property="og:image:height" content="630" /> */}
+    <meta property="og:image" content={og?.image ?? image} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
 
     {/* Twitter */}
-    {/* <meta name="twitter:card" content={twitter.card ?? 'summary_large_image'} /> */}
-    <meta name="twitter:card" content="summary_large_image" />
-    {/* <meta name="twitter:url" content={twitter.url ?? url} /> */}
-    {/* <meta name="twitter:title" content={twitter.title ?? title} /> */}
-    <meta name="twitter:title" content={title} />
-    {/* <meta
+    <meta name="twitter:card" content={twitter.card ?? 'summary_large_image'} />
+    <meta name="twitter:url" content={twitter.url ?? url} />
+    <meta name="twitter:title" content={twitter.title ?? title} />
+    <meta
       name="twitter:description"
       content={twitter.description ?? description}
-    /> */}
-    <meta name="twitter:description" content={description} />
-    {/* <meta property="twitter:image" content={twitter.image ?? image} /> */}
-    <meta
-      property="twitter:image"
-      content={`${JOB_FRAME_URL}/api/jobs?id=${id}&tab=details`}
     />
-    {/* <meta name="twitter:site" content={twitter.site} /> */}
+    <meta property="twitter:image" content={twitter.image ?? image} />
+    <meta name="twitter:site" content={twitter.site} />
 
-    {/* <script
+    <script
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={ldJson}
       key="jobpost-jsonld"
       type="application/ld+json"
-    /> */}
+    />
   </Head>
 );
 
