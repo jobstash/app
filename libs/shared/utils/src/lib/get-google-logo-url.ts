c1a17c994b2e8b88506f881a0a5c5db6edac54c2
John Ballesteros <johnshift.dev@gmail.com>
2024-02-06 09:28:34 +0800

    refactor: handle invalid url for google logo favicon fetch

--- libs/shared/utils/src/lib/get-google-logo-url.ts
+++ libs/shared/utils/src/lib/get-google-logo-url.ts
@@ -1,2 +1,6 @@
+import { getWebsiteText } from './get-website-text';
+
 export const getGoogleLogoUrl = (url: string) =>
-  `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}&sz=128`;
+  `https://www.google.com/s2/favicons?domain=${
+    getWebsiteText(url).hostname
+  }&sz=128`;
