7baca56f9316d794286b2d0085b3fcc7387d70f4
John Ballesteros <johnshift.dev@gmail.com>
2024-03-05 20:21:18 +0800

    refactor: handle unsupported https image domains

--- libs/shared/utils/src/lib/get-google-logo-url.ts
+++ libs/shared/utils/src/lib/get-google-logo-url.ts
@@ -1,6 +1,14 @@
 import { getWebsiteText } from './get-website-text';
 
+const URL_PREFIX = 'https://www.google.com/s2/favicons?domain=';
+const URL_SUFFIX = '&sz=64';
+
 export const getGoogleLogoUrl = (url: string) =>
-  `https://www.google.com/s2/favicons?domain=${
-    getWebsiteText(url).hostname
-  }&sz=64`;
+  `${URL_PREFIX}${getWebsiteText(url).hostname}${URL_SUFFIX}`;
+
+export const getLogoUrlHttpsAlternative = (googleString: string) => {
+  const url = new URL(googleString);
+  const domain = url.searchParams.get('domain');
+
+  return `${URL_PREFIX}https://${domain}${URL_SUFFIX}`;
+};
