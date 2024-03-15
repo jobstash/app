1c628a8528bb00717cb0989b023beb1e64d20a6f
DuckDegen <duckdegen@gmail.com>
2024-03-15 10:00:54 +0100

    fix(copy): landing copy

--- libs/home/ui/src/lib/landing/sections/testimonial-section.tsx
+++ libs/home/ui/src/lib/landing/sections/testimonial-section.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { memo } from 'react';
 
-import { lato, TELEGRAM_URL } from '@jobstash/shared/core';
+import { DUCK_TELEGRAM_URL, lato, TELEGRAM_URL } from '@jobstash/shared/core';
 
 import { HomePageButton } from './buttons/home-page-button';
 import { GradientContainer } from './gradient-container';
@@ -27,20 +27,20 @@ const TestimonialSection = () => (
                   fill
                   alt="placeholder"
                   className="object-center object-cover rounded-full"
-                  src="https://placehold.co/600x400/EEE/31343C/png"
+                  src="https://twitter.com/0xJMG/photo"
                 />
               </div>
             </div>
             <p className="text-md text-white/75 text-left pb-">
-              PancakeSwap has the most users of any decentralized platform,
-              ever. And those users are now entrusting the platform with over
-              $3.4 billion in funds. Will you join them? 1.4 million users in
-              the last 30 days 55 million trades made in the last 30 days.
+              In a sea of job platforms, JobStash shines with its unique and
+              uncomplicated approach to job aggregation and led by an authentic
+              team that genuinely want to help match the best talent. Really
+              cool UX and free!
             </p>
             <hr className="border-t border-white/20 mb-3 mt-6" />
-            <p className="text-white font-semibold text-md">Santiago Perez</p>
+            <p className="text-white font-semibold text-md">James Glasscock</p>
             <p className="text-white font-semibold text-md">
-              Crypto Company | Innovation Consultant
+              Head of Ecosystem, Reserve Protocol
             </p>
             <div className="w-[40px] mt-3">
               <div className="aspect-w-16 aspect-h-9">
@@ -48,13 +48,13 @@ const TestimonialSection = () => (
                   fill
                   alt="placeholder"
                   className="object-center object-cover "
-                  src="https://placehold.co/600x400/EEE/31343C/png"
+                  src="https://twitter.com/reserveprotocol/photo"
                 />
               </div>
             </div>
           </div>
         </div>
-        <div className="w-full md:w-1/2 px-2">
+        {/* <div className="w-full md:w-1/2 px-2">
           <div className="rounded-3xl bg-white bg-opacity-5 p-5">
             <div className="h-[60px] w-[60px] mx-auto mb-6">
               <div className="aspect-w-1 aspect-h-1">
@@ -90,25 +90,31 @@ const TestimonialSection = () => (
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
 
     <GradientContainer className="mt-16">
       <div className="grow text-white gap-4 flex flex-col">
         <span className={`${lato.className} text-xl font-bold`}>
-          Want to Get Featured or Boost Your Jobs?
+          Did you Hire or Get Hired Using JobStash ?
         </span>
         <span className="text-md text-white/75">
-          Drop us a message and we will make it happen.
+          Drop us a message and tell us how it went!
         </span>
       </div>
-      <HomePageButton hasBorder text="Contact" onClick={openTelegram} />
+      <HomePageButton hasBorder text="Contact" onClick={openDuckTelegram} />
     </GradientContainer>
   </section>
 );
 
 export default memo(TestimonialSection);
+
+const openDuckTelegram = () => {
+  if (typeof window !== 'undefined') {
+    window.open(DUCK_TELEGRAM_URL, '_blank');
+  }
+};
 
 const openTelegram = () => {
   if (typeof window !== 'undefined') {
