9a56f9a555a2c3397bfff37d0ff263c5c6d5e154
John Ballesteros <johnshift.dev@gmail.com>
2024-03-15 17:25:20 +0800

    refactor: add twitter to whitelisted image hosts

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -67,6 +67,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'twitter.com',
+      },
     ],
   },
 
