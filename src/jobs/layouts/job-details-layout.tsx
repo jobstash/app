7a9835341887a99a1d853ac4c1edae3c598504bc
John Ballesteros <johnshift.dev@gmail.com>
2024-03-19 10:45:19 +0800

    refactor: shared details-panel mobile-header component

--- src/jobs/layouts/job-details-layout.tsx
+++ src/jobs/layouts/job-details-layout.tsx
@@ -2,8 +2,7 @@ import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
 import { HREFS } from '~/shared/core/constants';
 import { getQueryClient } from '~/shared/utils/get-query-client';
-import { DetailsPanelBackButton } from '~/shared/components/details-panel/back-button';
-import { MobileHeader } from '~/shared/components/mobile-header';
+import { MobileHeader } from '~/shared/components/details-panel/mobile-header';
 import { PageScrollDisableSyncer } from '~/shared/components/page-scroll-syncer';
 
 import { jobQueryKeys } from '~/jobs/core/query-keys';
@@ -55,10 +54,7 @@ export const JobDetailsLayout = async ({ children, id }: Props) => {
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <div className="hide-scrollbar fixed right-0 top-0 z-20 size-full min-h-screen overflow-y-auto bg-darkest-gray pt-[68px] md:w-[calc((100%-212px))] md:pt-20 lg:w-[calc((100%-212px)/2)] lg:pt-0">
-        <MobileHeader
-          left={<DetailsPanelBackButton href={HREFS.JOBS_PAGE} />}
-          className="z-50 bg-darkest-gray md:left-auto md:right-0 md:flex md:w-[calc((100%-220px))] lg:hidden"
-        />
+        <MobileHeader href={HREFS.JOBS_PAGE} />
         <div className="flex flex-col gap-4 p-5 lg:p-6">
           <InitJobDetailsSyncer id={id} />
           <JobDetailsPanelHeader id={id} />
