486fdffc438d96aab0f68e1efa03318b52cf01b4
John Ballesteros <johnshift.dev@gmail.com>
2024-03-25 08:34:38 +0800

    refactor: change org url to be nullable
    Completes JOB-574

--- libs/shared/utils/src/lib/get-logo-url.ts
+++ libs/shared/utils/src/lib/get-logo-url.ts
@@ -1,7 +1,7 @@
 import { getGoogleLogoUrl } from './get-google-logo-url';
 import { getWebsiteText } from './get-website-text';
 
-export const getLogoUrl = (url: string, logo?: string | null) =>
+export const getLogoUrl = (url: string | null, logo?: string | null) =>
   logo && logo.trim().length > 0
     ? `/api/image?logo=${getWebsiteText(logo).link}&url=${
         getWebsiteText(url).link
