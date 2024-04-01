7f9a200c66cb69ca6e9db56db0c2f89bd67ba548
John Ballesteros <johnshift.dev@gmail.com>
2024-04-01 19:19:52 +0800

    fix: filters autohide on scroll down
    Resolves JOB-598

--- libs/filters/feature/src/lib/filters-wrapper.tsx
+++ libs/filters/feature/src/lib/filters-wrapper.tsx
@@ -12,46 +12,36 @@ interface Props {
 }
 
 const FiltersWrapper = ({ children }: Props) => {
-  const [showFilters, setShowFilters] = useAtom(showFiltersAtom);
-
-  const [scrollDirection, setScrollDirection] = useState<'up' | 'down' | null>(
-    null,
-  );
-
+  const [, setShowFilters] = useAtom(showFiltersAtom);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
   useEffect(() => {
-    let lastScrollY = window.pageYOffset;
-    const isLargerOffset = showFilters && scrollDirection === 'up';
-    const diffOffset = isLargerOffset ? 30 : 15;
+    let debounceTimer: NodeJS.Timeout;
 
-    const updateScrollDirection = () => {
-      const scrollY = window.pageYOffset;
-      const direction = scrollY > lastScrollY ? 'down' : 'up';
-      if (
-        direction !== scrollDirection &&
-        (scrollY - lastScrollY > diffOffset ||
-          scrollY - lastScrollY < -diffOffset)
-      ) {
-        setScrollDirection(direction);
-      }
+    const handleScroll = () => {
+      const currentScrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
 
-      lastScrollY = scrollY > 0 ? scrollY : 0;
+      clearTimeout(debounceTimer);
+
+      debounceTimer = setTimeout(() => {
+        if (
+          currentScrollTop > lastScrollTop &&
+          currentScrollTop - lastScrollTop > HIDE_THRESHOLD
+        ) {
+          setShowFilters(false);
+        }
+
+        setLastScrollTop(currentScrollTop);
+      }, DEBOUNCE_MS);
     };
 
-    window.addEventListener('scroll', updateScrollDirection);
+    window.addEventListener('scroll', handleScroll);
+
     return () => {
-      window.removeEventListener('scroll', updateScrollDirection);
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(debounceTimer);
     };
-  }, [scrollDirection, showFilters]);
-
-  useEffect(() => {
-    let hideTimeout: NodeJS.Timeout;
-
-    if (scrollDirection === 'down' && showFilters) {
-      hideTimeout = setTimeout(() => setShowFilters(false), 300);
-    }
-
-    return () => clearTimeout(hideTimeout);
-  }, [scrollDirection, setShowFilters, showFilters]);
+  }, [lastScrollTop, setShowFilters]);
 
   const isOpenTopBanner = useAtomValue(isOpenTopBannerAtom);
 
@@ -60,10 +50,6 @@ const FiltersWrapper = ({ children }: Props) => {
       className={cn(
         'fixed left-0 right-0 px-4 flex flex-col py-4 gap-y-4 bg-[#121216] z-40 transition-all duration-1000 lg:px-8 lg:items-center lg:py-0 lg:gap-x-4 lg:pt-0 lg:flex-row lg:relative lg:flex lg:right-auto lg:left-auto',
         { 'top-[108px] sm:top-[92px] lg:top-10': isOpenTopBanner },
-        // {
-        //   '-top-60 lg:-top-44': scrollDirection === 'down' && !showFilters,
-        // },
-        // { 'w-[calc(100%+32px)] overflow-x-hidden': showFilters && !isMobile },
       )}
     >
       {children}
@@ -72,3 +58,6 @@ const FiltersWrapper = ({ children }: Props) => {
 };
 
 export default FiltersWrapper;
+
+const DEBOUNCE_MS = 100;
+const HIDE_THRESHOLD = 200;
