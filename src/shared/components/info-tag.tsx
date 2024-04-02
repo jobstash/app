f4ecdcf952de96902d044f371bec5cabfbefa63f
John Ballesteros <89079900+johnshift@users.noreply.github.com>
2024-04-02 16:08:57 +0800

    refactor: adjust org-card link cardset bg color (#132)

--- src/shared/components/info-tag.tsx
+++ src/shared/components/info-tag.tsx
@@ -53,7 +53,7 @@ export const InfoTag = ({ tag, compact }: Props) => {
 const DEFAULT_CLASSNAME =
   'flex h-6 shrink-0 items-center gap-x-2 rounded-[4px] py-1 pr-2 lg:rounded-md';
 const LINK_CLASSNAME =
-  'bg-darker-gray cursor-pointer hover:bg-dark-gray duration-300 transition-all px-2';
+  'bg-white/10 hover:bg-white/40 cursor-pointer duration-300 transition-all px-2';
 const COMPACT_CLASSNAME = 'h-10 sm:h-12 lg:h-6';
 
 export const InfoTagSkeleton = ({ compact }: Pick<Props, 'compact'>) => {
