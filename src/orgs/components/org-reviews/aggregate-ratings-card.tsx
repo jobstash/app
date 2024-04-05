c4a96ccda88e8f74dfddde4df8e59c768eeb9b9d
John Ballesteros <johnshift.dev@gmail.com>
2024-04-05 16:27:07 +0800

    refactor: adjust aggregate-rating responsive layout

--- src/orgs/components/org-reviews/aggregate-ratings-card.tsx
+++ src/orgs/components/org-reviews/aggregate-ratings-card.tsx
@@ -30,22 +30,24 @@ export const AggregateRatingsCard = ({
 
       {showInfos && <>{infoTexts}</>}
 
-      <div className="grid-cold-1 2xl:grid-cols-3 grid gap-3 sm:grid-cols-2">
+      <div className="2xl:grid-cols-3 grid grid-cols-1 gap-6 sm:grid-cols-2 md:gap-4">
         {ratings.map(([label, rating]) => (
           <div
             key={label}
-            className="flex shrink-0 flex-wrap items-center justify-start gap-2"
+            className="flex shrink-0 flex-col flex-wrap items-start justify-start gap-2 md:flex-row md:items-center"
           >
-            <Text
-              className="text-base font-bold text-white"
-              text={rating.toFixed(1)}
-            />
-            <Rating
-              readOnly
-              value={rating}
-              style={{ maxWidth: 120 }}
-              itemStyles={RATING_ITEM_STYLES}
-            />
+            <div className="flex items-center gap-2">
+              <Text
+                className="text-base font-bold text-white"
+                text={rating.toFixed(1)}
+              />
+              <Rating
+                readOnly
+                value={rating}
+                style={{ maxWidth: 120 }}
+                itemStyles={RATING_ITEM_STYLES}
+              />
+            </div>
 
             <Text
               className="shrink-0 text-base text-white"
