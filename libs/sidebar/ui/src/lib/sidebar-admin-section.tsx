0be20589e7a0c24b12d1b1eb1b381a1f62c1f571
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2024-04-05 15:48:02 +0200

    better menu mobile animation

--- libs/sidebar/ui/src/lib/sidebar-admin-section.tsx
+++ libs/sidebar/ui/src/lib/sidebar-admin-section.tsx
@@ -1,33 +1,44 @@
 import { ADMIN_PATHS, GODMODE_PATH_PREFIX } from '@jobstash/admin/core';
+import { cn } from '@jobstash/shared/utils';
 
 import { Text } from '@jobstash/shared/ui';
 
 import SidebarBartab from './sidebar-bartab';
-
-const SidebarAdminSection = () => (
-  <div className="flex-col">
-    <Text color="dimmed">Admin Tasks</Text>
-    <div className="space-y-3 pt-3">
-      <SidebarBartab path={ADMIN_PATHS.ORG_APPROVALS} text="Org Approvals" />
-      <SidebarBartab
-        path={ADMIN_PATHS.SYNONYMS}
-        text="Tags"
-        isActiveFn={(pathname) =>
-          pathname.slice(0, GODMODE_PATH_PREFIX.TAGS.length) ===
-          GODMODE_PATH_PREFIX.TAGS
-        }
-      />
-      <SidebarBartab path={ADMIN_PATHS.ORG_LIST} text="Organizations" />
-      <SidebarBartab
-        path={ADMIN_PATHS.ALL_JOBS}
-        text="All Jobs"
-        isActiveFn={(pathname) =>
-          pathname.slice(0, GODMODE_PATH_PREFIX.ALL_JOBS.length) ===
-          GODMODE_PATH_PREFIX.ALL_JOBS
-        }
-      />
+interface Props {
+  isMobile?: boolean;
+}
+const SidebarAdminSection = ({ isMobile }: Props) => {
+  const wrapperClassName = cn('space-y-2 pt-3', {
+    'flex flex-col justify-start items-start [&>*]:bg-transparent [&>*]:bg-none [&>*]:hover:bg-transparent':
+      isMobile,
+  });
+  return (
+    <div className={wrapperClassName}>
+      <Text color="dimmed">Admin Tasks</Text>
+      <div className="space-y-3 pt-3">
+        <SidebarBartab isMobile={isMobile} path={ADMIN_PATHS.ORG_APPROVALS} text="Org Approvals" />
+        <SidebarBartab
+          path={ADMIN_PATHS.SYNONYMS}
+          text="Tags"
+          isMobile={isMobile}
+          isActiveFn={(pathname) =>
+            pathname.slice(0, GODMODE_PATH_PREFIX.TAGS.length) ===
+            GODMODE_PATH_PREFIX.TAGS
+          }
+        />
+        <SidebarBartab isMobile={isMobile} path={ADMIN_PATHS.ORG_LIST} text="Organizations" />
+        <SidebarBartab
+          isMobile={isMobile}
+          path={ADMIN_PATHS.ALL_JOBS}
+          text="All Jobs"
+          isActiveFn={(pathname) =>
+            pathname.slice(0, GODMODE_PATH_PREFIX.ALL_JOBS.length) ===
+            GODMODE_PATH_PREFIX.ALL_JOBS
+          }
+        />
+      </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default SidebarAdminSection;
