7c8f23d65f48703b99ed75ad692f9b5680ee3288
DuckDegen <duckdegen@gmail.com>
2024-06-21 12:13:41 +0200

    fix(copy): copy

--- libs/right-panel/ui/src/lib/crypto-native-job-cta/eligible-tooltip-content.tsx
+++ libs/right-panel/ui/src/lib/crypto-native-job-cta/eligible-tooltip-content.tsx
@@ -4,15 +4,18 @@ export const EligibleTooltipContent = () => (
   <div className="flex flex-col gap-8 max-w-sm p-4 bg-darker-gray">
     <div className="flex flex-col gap-2">
       <Text fw="bold" size="lg">
-        Fast-Track Apply
+        Elite Fast Track Apply
       </Text>
       <Text className="text-white/70">
-        You are eligible for Fast-Track application! Apply quickly and
-        efficiently to get noticed by hiring managers.
+        Congratulations! You are eligible for Elite Fast Track application! Upon
+        filling out the application form, you will be introduced to the
+        organization using a different process which will put you at a
+        significant advantage over other applicants.
       </Text>
       <Text className="text-white/70">
-        Benefit from our streamlined application process and stand out to
-        employers looking for top talent.
+        We take a lot of pride in supporting builders like you by giving you
+        priority access to some of the best opportunities in the space. Your
+        application through our partners allows us to fund our operations.
       </Text>
     </div>
   </div>
