d3dcc25ee6bf81f17913e33de9a1929dddfb6aea
John Ballesteros <johnshift.dev@gmail.com>
2024-07-01 13:39:58 +0800

    refactor: contact information form WIP

--- libs/profile/ui/src/lib/profile-header/profile-header-contact-info.tsx
+++ libs/profile/ui/src/lib/profile-header/profile-header-contact-info.tsx
@@ -1,16 +1,78 @@
-import ProfileHeaderCityInput from './profile-header-city-input';
-import ProfileHeaderCountryInput from './profile-header-country-input';
-import ProfileHeaderLayout from './profile-header-layout';
-import ProfileHeaderPreferredContactInput from './profile-header-preferred-contact-input';
-import ProfileHeaderSaveButton from './profile-header-save-button';
-import ProfileHeaderSelectedContactInput from './profile-header-selected-contact-input';
+import { useState } from 'react';
 
-export const ProfileHeaderContactInfo = () => (
-  <ProfileHeaderLayout
-    preferredInput={<ProfileHeaderPreferredContactInput />}
-    selectedInput={<ProfileHeaderSelectedContactInput />}
-    countryInput={<ProfileHeaderCountryInput />}
-    cityInput={<ProfileHeaderCityInput />}
-    saveButton={<ProfileHeaderSaveButton />}
-  />
-);
+import { Button, Input } from '@nextui-org/react';
+
+import { capitalize, notifError } from '@jobstash/shared/utils';
+
+const FIELDS = [
+  'email',
+  'discord',
+  'telegram',
+  'farcaster',
+  'lens',
+  'twitter',
+  'city',
+  'country',
+];
+
+interface FormData {
+  email: string;
+  discord: string;
+  telegram: string;
+  farcaster: string;
+  lens: string;
+  twitter: string;
+}
+
+export const ProfileHeaderContactInfo = () => {
+  const [formData, setFormData] = useState<FormData>({
+    email: '',
+    discord: '',
+    telegram: '',
+    farcaster: '',
+    lens: '',
+    twitter: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Form Data Submitted:', formData);
+
+    const isAnyFieldFilled = Object.values(formData).some(
+      (field) => field.trim() !== '',
+    );
+
+    if (!isAnyFieldFilled) {
+      notifError({
+        title: 'Submission Failed!',
+        message: 'Please fill at least one field to save your contact info',
+      });
+    }
+  };
+
+  return (
+    <form
+      className="grid md:grid-cols-2 gap-6 pt-0 p-2 pb-4"
+      onSubmit={handleSubmit}
+    >
+      {FIELDS.map((name, i) => (
+        <div key={name} className="h-10 min-h-[40px]">
+          <Input
+            name={name}
+            label={capitalize(name)}
+            size="sm"
+            onChange={handleChange}
+          />
+        </div>
+      ))}
+      <Button type="submit" className="w-fit">
+        Save
+      </Button>
+    </form>
+  );
+};
