5bf83f6b2aa795dd98f0c05cafc0b2a1ccb1af81
John Ballesteros <johnshift.dev@gmail.com>
2024-07-15 14:26:27 +0800

    refactor: restore auth guard

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,53 +1,50 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-// TEMP DISABLED
-// import { ENABLE_BASIC_AUTH } from '@jobstash/shared/core';
+import { ENABLE_BASIC_AUTH } from '@jobstash/shared/core';
 
-// TEMP DISABLED
-// const whiteListed = [
-//   'jobs/ava-labs-senior-infrastructure-engineer-3oxfCO/details',
-//   'JobStash.svg',
-//   'favicon.ico',
-//   '_next',
-//   'Logo-01.svg',
-//   'Logo-02.svg',
-//   'JobStash-Wordmark-800.png',
-//   'JobStash.svg',
-//   'site.webmanifest',
-// ];
+const whiteListed = [
+  'jobs/ava-labs-senior-infrastructure-engineer-3oxfCO/details',
+  'JobStash.svg',
+  'favicon.ico',
+  '_next',
+  'Logo-01.svg',
+  'Logo-02.svg',
+  'JobStash-Wordmark-800.png',
+  'JobStash.svg',
+  'site.webmanifest',
+];
 
-// TEMP DISABLED
-// const getIsWhiteListed = (url: string) => {
-//   for (const str of whiteListed) {
-//     if (url.includes(str)) return true;
-//   }
+const getIsWhiteListed = (url: string) => {
+  for (const str of whiteListed) {
+    if (url.includes(str)) return true;
+  }
 
-//   return false;
-// };
+  return false;
+};
 
-export const middleware = (req: NextRequest) =>
-  // TEMP DISABLED
-  // if (typeof ENABLE_BASIC_AUTH === 'string' && ENABLE_BASIC_AUTH === 'true') {
-  //   const basicAuth = req.headers.get('authorization');
-  //   const url = req.nextUrl;
-  //   const isWhiteListed = getIsWhiteListed(url.toString());
+export function middleware(req: NextRequest) {
+  if (typeof ENABLE_BASIC_AUTH === 'string' && ENABLE_BASIC_AUTH === 'true') {
+    const basicAuth = req.headers.get('authorization');
+    const url = req.nextUrl;
+    const isWhiteListed = getIsWhiteListed(url.toString());
 
-  //   if (isWhiteListed) {
-  //     return NextResponse.next();
-  //   }
+    if (isWhiteListed) {
+      return NextResponse.next();
+    }
 
-  //   if (basicAuth) {
-  //     const authValue = basicAuth.split(' ')[1];
-  //     const [user, pwd] = atob(authValue).split(':');
+    if (basicAuth) {
+      const authValue = basicAuth.split(' ')[1];
+      const [user, pwd] = atob(authValue).split(':');
 
-  //     if (user === 'gotrekt' && pwd === 'mcdonalds') {
-  //       return NextResponse.next();
-  //     }
-  //   }
+      if (user === 'gotrekt' && pwd === 'mcdonalds') {
+        return NextResponse.next();
+      }
+    }
 
-  //   url.pathname = '/api/auth';
+    url.pathname = '/api/auth';
 
-  //   return NextResponse.rewrite(url);
-  // }
+    return NextResponse.rewrite(url);
+  }
 
-  NextResponse.next();
+  return NextResponse.next();
+}
