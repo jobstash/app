5d02bcfcb094579d214653d8ce36596f86af23a9
John Ballesteros <johnshift.dev@gmail.com>
2024-07-15 13:29:07 +0800

    refactor: temp disable dev auth guard

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,50 +1,53 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { ENABLE_BASIC_AUTH } from '@jobstash/shared/core';
+// TEMP DISABLED
+// import { ENABLE_BASIC_AUTH } from '@jobstash/shared/core';
 
-const whiteListed = [
-  'jobs/ava-labs-senior-infrastructure-engineer-3oxfCO/details',
-  'JobStash.svg',
-  'favicon.ico',
-  '_next',
-  'Logo-01.svg',
-  'Logo-02.svg',
-  'JobStash-Wordmark-800.png',
-  'JobStash.svg',
-  'site.webmanifest',
-];
+// TEMP DISABLED
+// const whiteListed = [
+//   'jobs/ava-labs-senior-infrastructure-engineer-3oxfCO/details',
+//   'JobStash.svg',
+//   'favicon.ico',
+//   '_next',
+//   'Logo-01.svg',
+//   'Logo-02.svg',
+//   'JobStash-Wordmark-800.png',
+//   'JobStash.svg',
+//   'site.webmanifest',
+// ];
 
-const getIsWhiteListed = (url: string) => {
-  for (const str of whiteListed) {
-    if (url.includes(str)) return true;
-  }
+// TEMP DISABLED
+// const getIsWhiteListed = (url: string) => {
+//   for (const str of whiteListed) {
+//     if (url.includes(str)) return true;
+//   }
 
-  return false;
-};
+//   return false;
+// };
 
-export function middleware(req: NextRequest) {
-  if (typeof ENABLE_BASIC_AUTH === 'string' && ENABLE_BASIC_AUTH === 'true') {
-    const basicAuth = req.headers.get('authorization');
-    const url = req.nextUrl;
-    const isWhiteListed = getIsWhiteListed(url.toString());
+export const middleware = (req: NextRequest) =>
+  // TEMP DISABLED
+  // if (typeof ENABLE_BASIC_AUTH === 'string' && ENABLE_BASIC_AUTH === 'true') {
+  //   const basicAuth = req.headers.get('authorization');
+  //   const url = req.nextUrl;
+  //   const isWhiteListed = getIsWhiteListed(url.toString());
 
-    if (isWhiteListed) {
-      return NextResponse.next();
-    }
+  //   if (isWhiteListed) {
+  //     return NextResponse.next();
+  //   }
 
-    if (basicAuth) {
-      const authValue = basicAuth.split(' ')[1];
-      const [user, pwd] = atob(authValue).split(':');
+  //   if (basicAuth) {
+  //     const authValue = basicAuth.split(' ')[1];
+  //     const [user, pwd] = atob(authValue).split(':');
 
-      if (user === 'gotrekt' && pwd === 'mcdonalds') {
-        return NextResponse.next();
-      }
-    }
+  //     if (user === 'gotrekt' && pwd === 'mcdonalds') {
+  //       return NextResponse.next();
+  //     }
+  //   }
 
-    url.pathname = '/api/auth';
+  //   url.pathname = '/api/auth';
 
-    return NextResponse.rewrite(url);
-  }
+  //   return NextResponse.rewrite(url);
+  // }
 
-  return NextResponse.next();
-}
+  NextResponse.next();
