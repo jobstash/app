16a1cd361c683540241319889ac232123df38f22
John Ballesteros <johnshift.dev@gmail.com>
2024-07-16 19:31:36 +0800

    refactor: remove unused page

--- apps/web/pages/profile/org/talentsx.tsx
+++
@@ -1,1 +0,0 @@
-export { ProfileOrgTalentsPage as default } from '@jobstash/profile/pages';
--- libs/profile/pages/src/index.ts
+++ libs/profile/pages/src/index.ts
@@ -2,7 +2,6 @@ export * from './lib/applicants-page';
 export * from './lib/ats-oath-lever-callback-page';
 export * from './lib/ats-provider-error-page';
 export * from './lib/ats-settings-page';
-export * from './lib/profile-org-talents-page';
 export * from './lib/profile-page';
 export * from './lib/profile-repositories-page';
 export * from './lib/profile-reviews-page';
