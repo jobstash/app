aab6504283f24f3f078ea72328ffce9da681b314
cristiano <cristianocardelli@gmail.com>
2024-07-22 18:35:07 +0200

    Feature/job 646 663 (#154)
    * reduce width elements
    
    * fix search bar in header - fix active featured card
    
    ---------
    
    Co-authored-by: cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>

--- libs/filters/ui/src/lib/search-filter.tsx
+++ libs/filters/ui/src/lib/search-filter.tsx
@@ -21,38 +21,29 @@ const SearchFilter = () => {
   const searchQuery = state?.filterValues?.query;
 
   return (
-    <form onSubmit={onSubmitSearch}>
-      <TextInput
-        icon={<SearchInputIcon />}
-        placeholder={`Search ${capitalize(routeSection.slice(1))}`}
-        size="lg"
-        rightSectionWidth={140}
-        rightSection={
-          <div className="hidden items-center gap-x-2 lg:flex">
-            {/* <Button isIcon isDisabled={isLoading} onClick={clearSearch}>
-              <CloseIcon />
-            </Button> */}
-            <Button type="submit" isDisabled={isLoading}>
-              Search
-            </Button>
-          </div>
-        }
-        value={searchQuery ?? ''}
-        disabled={isLoading}
-        radius="md"
-        styles={{
-          input: {
-            background: 'rgba(255, 255, 255, 0.1)',
-            fontSize: 16,
-            border: 'transparent',
-          },
-        }}
-        classNames={{
-          input: 'py-7 bg-white/10',
-        }}
-        onChange={onChangeSearch}
-      />
-    </form>
+    <div className='py-4 border-b border-white lg:border-transparent'>
+      <form onSubmit={onSubmitSearch}>
+        <TextInput
+          icon={<SearchInputIcon />}
+          placeholder={`Search ${capitalize(routeSection.slice(1))}`}
+          size="25"
+          value={searchQuery ?? ''}
+          disabled={isLoading}
+          radius="md"
+          styles={{
+            input: {
+              background: 'transparent',
+              fontSize: 16,
+              border: 'transparent',
+            },
+          }}
+          classNames={{
+            input: 'pl-10 bg-transparent text-white placeholder:text-white',
+          }}
+          onChange={onChangeSearch}
+        />
+      </form>
+    </div>
   );
 };
 
