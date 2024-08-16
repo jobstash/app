aacbb9918b79506180a8f6336118243314f3d673
DuckDegen <duckdegen@gmail.com>
2024-08-16 12:11:53 +0200

    improving copy

--- libs/right-panel/ui/src/lib/crypto-native-job-cta/anon-tooltip-content.tsx
+++ libs/right-panel/ui/src/lib/crypto-native-job-cta/anon-tooltip-content.tsx
@@ -4,16 +4,21 @@ export const AnonTooltipContent = () => (
   <div className="flex flex-col gap-8 max-w-sm p-4 bg-darker-gray">
     <div className="flex flex-col gap-2">
       <Text fw="bold" size="lg">
-        Check Job Concierge Access
+        Check if you qualify as Expert
       </Text>
       <Text className="text-white/70">
-        Find out if you are eligible for our Job Concierge application process
-        and get connected with hiring managers quickly. To get started, connect
-        your wallet and your github account, or verify your current work email.
+        Qualifying candidates get free personalized advice on how to
+        successfully apply for this job, and have us advocate for you with the
+        hiring manager. To find out if you are eligible, connect your wallet and
+        your github account, or verify your current work email, we will check
+        your work history and automatically approve you if you qualify.
       </Text>
       <Text className="text-white/70">
-        We support crypto native builders by giving them priority access to some
-        of the best opportunities in the space.
+        Qualification criteria: You must have verified OSS contributions in a
+        repo which is owned by one of the organizations we track, or have a
+        verified work email from one of the organizations we track. We also
+        Hackathon NFT Packs issued by EthGlobal, and accept any tier above and
+        including the Pioneer tier.
       </Text>
     </div>
   </div>
