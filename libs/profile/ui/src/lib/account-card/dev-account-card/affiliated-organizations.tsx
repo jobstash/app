3a60843daba21baa959b894015a04492963e3c44
John Ballesteros <johnshift.dev@gmail.com>
2024-09-30 13:40:01 +0800

    refactor: add affiliated organizations manage actions

--- libs/profile/ui/src/lib/account-card/dev-account-card/affiliated-organizations.tsx
+++ libs/profile/ui/src/lib/account-card/dev-account-card/affiliated-organizations.tsx
@@ -1,22 +1,39 @@
-import { Spinner } from '@nextui-org/react';
+import { Button, Spinner, Tooltip } from '@nextui-org/react';
+import { SlidersHorizontal as ManageOrgIcon } from 'lucide-react';
 
-import { getGoogleLogoUrl } from '@jobstash/shared/utils';
-
-import { useAffiliatedOrgs } from '@jobstash/auth/state';
+import { getGoogleLogoUrl, getPluralText } from '@jobstash/shared/utils';
 
 import { LogoTitle, Text } from '@jobstash/shared/ui';
 
+import { EditIcon, LeftIcon } from './icons';
+import { useAffiliatedOrganizations } from './use-affiliated-organizations';
+
 export const AffiliatedOrganizations = () => {
-  const { data } = useAffiliatedOrgs();
+  const { data, isEditing, toggleEdit, orgCount, hasOrg, getOnManageFn } =
+    useAffiliatedOrganizations();
 
   return (
     <>
       <hr className="border-t border-white/10" />
 
       <div className="flex flex-col gap-2">
-        <Text size="lg" fw="bold">
-          Affiliated Organizations
-        </Text>
+        <div className="flex items-center justify-between">
+          <Text size="lg" fw="bold">
+            {`Affiliated ${getPluralText('Organization', orgCount)}`}
+          </Text>
+          {hasOrg && (
+            <div>
+              <Button
+                size="sm"
+                variant="flat"
+                startContent={isEditing ? <LeftIcon /> : <EditIcon />}
+                onClick={toggleEdit}
+              >
+                {isEditing ? 'Done' : 'Manage Affiliations'}
+              </Button>
+            </div>
+          )}
+        </div>
 
         <Text color="dimmed">
           These are the organizations associated with your accounts. Unlock
@@ -26,18 +43,36 @@ export const AffiliatedOrganizations = () => {
 
       {data ? (
         <div className="flex flex-col gap-6 pl-4">
-          {data.map(({ id, name, url, logo, account }) => (
-            <LogoTitle
+          {data.map(({ id, name, url, logo, account, slug }) => (
+            <div
               key={id}
-              title={name}
-              avatarProps={{
-                src: logo ?? getGoogleLogoUrl(url),
-                alt: name,
-                isRounded: true,
-                name,
-              }}
-              location={account}
-            />
+              className="flex items-center justify-between w-full max-w-[260px]"
+            >
+              <LogoTitle
+                title={name}
+                avatarProps={{
+                  src: logo ?? getGoogleLogoUrl(url),
+                  alt: name,
+                  isRounded: true,
+                  name,
+                }}
+                location={account}
+              />
+              {isEditing && (
+                <div>
+                  <Tooltip content="Manage Organization">
+                    <Button
+                      isIconOnly
+                      size="sm"
+                      variant="flat"
+                      onClick={getOnManageFn(slug)}
+                    >
+                      <ManageOrgIcon className="text-white/80" size={20} />
+                    </Button>
+                  </Tooltip>
+                </div>
+              )}
+            </div>
           ))}
         </div>
       ) : (
---
+++ libs/profile/ui/src/lib/account-card/dev-account-card/use-affiliated-organizations.ts
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router';
+import { useReducer } from 'react';
+
+import { useAffiliatedOrgs } from '@jobstash/auth/state';
+
+export const useAffiliatedOrganizations = () => {
+  const [isEditing, toggleEdit] = useReducer((prev) => !prev, false);
+
+  const { data } = useAffiliatedOrgs();
+
+  const orgCount = (data ?? []).length;
+  const hasOrg = orgCount > 0;
+
+  const router = useRouter();
+
+  const getOnManageFn = (slug: string) => () => {
+    router.push(`/profile/organizations/${slug}`);
+  };
+
+  return {
+    isEditing,
+    toggleEdit,
+    data,
+    orgCount,
+    hasOrg,
+    getOnManageFn,
+  };
+};
