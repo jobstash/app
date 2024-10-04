c95ca5517e41eff90d440ac2689355dee5936cee
John Ballesteros <johnshift.dev@gmail.com>
2024-10-04 13:02:55 +0800

    refactor: add spinner to filter-toggle button when loading

--- libs/filters/ui/src/lib/filters-toggle-button.tsx
+++ libs/filters/ui/src/lib/filters-toggle-button.tsx
@@ -1,5 +1,7 @@
 import { memo } from 'react';
 
+import { Spinner } from '@nextui-org/react';
+
 import { getToggleFilterText } from '@jobstash/filters/utils';
 
 import { useFiltersContext } from '@jobstash/filters/state';
@@ -10,10 +12,12 @@ const FiltersToggleButton = () => {
   const { showFilters, isLoading, filterCount, toggleFilters } =
     useFiltersContext();
 
+  const icon = isLoading ? <Spinner size="sm" color="white" /> : <FilterIcon />;
+
   return (
     <Button
       variant={filterCount > 0 ? 'primary' : 'outline'}
-      left={<FilterIcon />}
+      left={icon}
       isActive={showFilters}
       isDisabled={isLoading}
       onClick={toggleFilters}
