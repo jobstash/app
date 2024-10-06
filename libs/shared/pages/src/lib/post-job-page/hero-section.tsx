6ba52574305b4ab18226619ad0e084e2ad1c7682
DuckDegen <duckdegen@gmail.com>
2024-10-06 13:31:54 +0200

    fix hero copy

--- libs/shared/pages/src/lib/post-job-page/hero-section.tsx
+++ libs/shared/pages/src/lib/post-job-page/hero-section.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { ChevronRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@nextui-org/react';
 
 import { ORG_SIGNUP_FORM_URL } from '@jobstash/shared/core';
@@ -9,29 +8,14 @@ import { ORG_SIGNUP_FORM_URL } from '@jobstash/shared/core';
 export const HeroSection = () => (
   <div className="max-w-7xl px-6 pt-10 lg:flex lg:px-8">
     <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8 flex flex-col gap-8">
-      <div className="mt-24 sm:mt-32 lg:mt-16">
-        <a href="/jobs" className="inline-flex space-x-6">
-          <span className="rounded-full bg-secondary/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-secondary/20">
-            Active Jobs
-          </span>
-          <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-white/90">
-            <span>Checkout job listings</span>
-            <ChevronRightIcon
-              aria-hidden="true"
-              className="h-5 w-5 text-gray-500"
-            />
-          </span>
-        </a>
-      </div>
-
-      <div className="flex flex-col gap-2">
+      <div className="mt-24 sm:mt-32 lg:mt-16 flex flex-col gap-2">
         <h1 className="bg-gradient-to-r from-white to-secondary quinary bg-clip-text text-8xl font-bold text-transparent">
-          Find Your Next Great Hire
+          Post jobs for your organization
         </h1>
         <div className="flex gap-2 items-center">
           <h2 className="bg-gradient-to-r from-white to-secondary quinary bg-clip-text text-3xl font-bold text-transparent">
             for{' '}
-            <span className="text-5xl font-bold bg-gradient-to-r from-secondary/10 via-secondary/40 to-[#D68800]/80 bg-clip-text text-3xl font-bold text-transparent">
+            <span className="bg-gradient-to-r from-secondary/10 via-secondary/40 to-[#D68800]/80 bg-clip-text text-3xl font-bold text-transparent">
               FREE
             </span>
           </h2>
@@ -42,9 +26,10 @@ export const HeroSection = () => (
       </div>
 
       <p className="text-xl leading-7 text-white/90">
-        Reach a vast network of talented professionals eager to join your team.
-        Post your job at <span className="font-bold">zero</span> cost and
-        connect with the right candidates quickly and efficiently.
+        We import and publish your jobs from any source fully automatically
+        without any required action from you. Post all your jobs at{' '}
+        <span className="font-bold">zero</span> cost and connect with the right
+        candidates quickly and efficiently.
       </p>
 
       <div className="flex items-center gap-x-6">
@@ -54,7 +39,7 @@ export const HeroSection = () => (
           className="bg-gradient-to-l from-primary to-tertiary font-bold"
           radius="sm"
         >
-          Post Your Job
+          Post Your Jobs For Free
         </Button>
         <Button variant="light" radius="sm" as={Link} href="/employers">
           Learn more <span aria-hidden="true">→</span>
