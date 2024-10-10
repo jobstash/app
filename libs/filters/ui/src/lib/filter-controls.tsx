0c1fa1bc33fcf02b3e45a8b24805706cab421288
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2024-10-10 14:14:19 +0200

    fix bookmark page layout

--- libs/filters/ui/src/lib/filter-controls.tsx
+++ libs/filters/ui/src/lib/filter-controls.tsx
@@ -4,7 +4,7 @@ import FiltersItemsCount from './filters-items-count';
 import FiltersToggleButton from './filters-toggle-button';
 
 const FilterControls = () => (
-  <div className="flex flex-wrap items-center justify-between gap-x-6 gap-y-2">
+  <div className="flex items-center justify-between gap-x-6 gap-y-2">
     <FiltersToggleButton />
     {/* <DesktopSortFilters /> */}
     <FiltersItemsCount />
