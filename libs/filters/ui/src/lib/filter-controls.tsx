03dfddc408426853e27657940327466e7ba3ab12
cristiano cardelli <cristianocardelli@gmail.com, cristiano@welance.com>
2024-10-11 14:37:53 +0200

    refactor header + search field + layout 2 columns

--- libs/filters/ui/src/lib/filter-controls.tsx
+++ libs/filters/ui/src/lib/filter-controls.tsx
@@ -1,13 +1,11 @@
 import { memo } from 'react';
 
-import FiltersItemsCount from './filters-items-count';
 import FiltersToggleButton from './filters-toggle-button';
 
 const FilterControls = () => (
   <div className="flex items-center justify-between gap-x-6 gap-y-2">
     <FiltersToggleButton />
     {/* <DesktopSortFilters /> */}
-    <FiltersItemsCount />
   </div>
 );
 
