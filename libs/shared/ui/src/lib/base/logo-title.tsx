dc93cc0fb555be8529293d608abf98600ea19892
John Ballesteros <johnshift.dev@gmail.com>
2024-10-14 16:22:09 +0800

    refactor: allow react-nodes in logo-title title prop

--- libs/shared/ui/src/lib/base/logo-title.tsx
+++ libs/shared/ui/src/lib/base/logo-title.tsx
@@ -19,7 +19,7 @@ const logoTitle = cva([], {
 type LogoTitleVariantProps = VariantProps<typeof logoTitle>;
 
 interface LogoTitleProps extends LogoTitleVariantProps {
-  title: string;
+  title: React.ReactNode;
   avatarProps: AvatarProps;
   location?: React.ReactNode;
   hasMinWidth?: boolean;
@@ -49,7 +49,11 @@ const LogoTitle = ({
       identiconFallback={identiconFallback}
     />
     <div className="flex flex-col justify-center gap-0.5">
-      <Heading size={size === 'lg' ? 'md' : 'sm'}>{title}</Heading>
+      {typeof title === 'string' ? (
+        <Heading size={size === 'lg' ? 'md' : 'sm'}>{title}</Heading>
+      ) : (
+        title
+      )}
       {location &&
         (typeof location === 'string' ? (
           <Text size="sm" color="dimmed">
@@ -63,57 +67,3 @@ const LogoTitle = ({
 );
 
 export default LogoTitle;
-
-//
-// import { cva, type VariantProps } from 'class-variance-authority';
-
-// import { cn } from '@jobstash/shared/utils';
-
-// import { type AvatarProps, default as Avatar } from './avatar';
-// import Heading from './heading';
-// import Text from './text';
-
-// const logoTitle = cva([], {
-//   variants: {
-//     size: {
-//       sm: '',
-//       md: '',
-//       lg: '',
-//     },
-//   },
-// });
-
-// type LogoTitleVariantProps = VariantProps<typeof logoTitle>;
-
-// interface LogoTitleProps extends LogoTitleVariantProps {
-//   title: string;
-//   avatarProps: AvatarProps;
-//   location?: string;
-//   hasMinWidth?: boolean;
-// }
-
-// const LogoTitle = ({
-//   title,
-//   avatarProps,
-//   size = 'md',
-//   location,
-//   hasMinWidth,
-// }: LogoTitleProps) => (
-//   <div
-//     className={cn('flex w-fit items-center gap-x-3', {
-//       'min-w-[148px]': hasMinWidth,
-//     })}
-//   >
-//     <Avatar key={avatarProps.src} {...avatarProps} />
-//     <div className="flex flex-col justify-center gap-1">
-//       <Heading size={size === 'lg' ? 'md' : 'sm'}>{title}</Heading>
-//       {location && (
-//         <Text size="sm" color="dimmed">
-//           {location}
-//         </Text>
-//       )}
-//     </div>
-//   </div>
-// );
-
-// export default LogoTitle;
