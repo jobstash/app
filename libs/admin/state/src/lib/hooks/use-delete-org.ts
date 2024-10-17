4941081bbf88db6ec9ae1723e2282147117e6864
John Ballesteros <johnshift.dev@gmail.com>
2024-10-17 17:04:18 +0800

    refactor: invalidate all-orgs fetch after delete

--- libs/admin/state/src/lib/hooks/use-delete-org.ts
+++ libs/admin/state/src/lib/hooks/use-delete-org.ts
@@ -1,8 +1,18 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
+import { useMwVersionContext } from '@jobstash/shared/state';
 import { deleteOrg } from '@jobstash/admin/data';
 
-export const useDeleteOrg = (orgId: string) =>
-  useMutation({
+export const useDeleteOrg = (orgId: string) => {
+  const queryClient = useQueryClient();
+  const { mwVersion } = useMwVersionContext();
+
+  return useMutation({
     mutationFn: () => deleteOrg(orgId),
+    onSuccess() {
+      queryClient.invalidateQueries({
+        queryKey: [mwVersion, 'all-orgs'],
+      });
+    },
   });
+};
