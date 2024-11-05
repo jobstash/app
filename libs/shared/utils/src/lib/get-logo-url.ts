835679817ad7eb8740e06a5096473af4f9ef9e03
John Ballesteros <johnshift.dev@gmail.com>
2024-11-05 22:25:35 +0800

    fix: blurred google favicon images

--- libs/shared/utils/src/lib/get-logo-url.ts
+++ libs/shared/utils/src/lib/get-logo-url.ts
@@ -1,9 +1,13 @@
 import { getGoogleLogoUrl } from './get-google-logo-url';
 import { getWebsiteText } from './get-website-text';
 
+const GOOGLE_FAVICON_SUBSTRING = 'www.google.com/s2/favicons';
+
 export const getLogoUrl = (url: string | null, logo?: string | null) =>
   logo && logo.trim().length > 0
-    ? `/api/image?logo=${getWebsiteText(logo).link}&url=${
-        getWebsiteText(url).link
-      }`
+    ? logo.includes(GOOGLE_FAVICON_SUBSTRING)
+      ? logo
+      : `/api/image?logo=${getWebsiteText(logo).link}&url=${
+          getWebsiteText(url).link
+        }`
     : getGoogleLogoUrl(url);
