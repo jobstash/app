17463347b89129eed6b9bec86fb8573aeb044a66
John Ballesteros <johnshift.dev@gmail.com>
2024-12-13 21:09:48 +0800

    fix: bookmarks nav flashing
    Fixes JOB-867

--- libs/sidebar/ui/src/lib/sidebar-bookmarks-section.tsx
+++ libs/sidebar/ui/src/lib/sidebar-bookmarks-section.tsx
@@ -4,6 +4,7 @@ import { useMemo } from 'react';
 import { PERMISSIONS } from '@jobstash/auth/core';
 
 import { useHasPermission } from '@jobstash/auth/state';
+import { useSavedJobs } from '@jobstash/jobs/state';
 
 import { BookmarkSidebarIcon } from '@jobstash/shared/ui';
 
@@ -15,7 +16,9 @@ interface Props {
 
 const SidebarBookmarksSection = ({ isMobile }: Props) => {
   const router = useRouter();
+  const { data: savedJobs } = useSavedJobs();
 
+  const hasSavedJobs = savedJobs && savedJobs.length > 0;
   const hasParam = Boolean(router.query.slug);
   const hasUserPermission = useHasPermission(PERMISSIONS.USER);
   const isAnonSavedJobs = !hasParam && !hasUserPermission;
@@ -36,7 +39,7 @@ const SidebarBookmarksSection = ({ isMobile }: Props) => {
     ];
   }, [hasParam, hasUserPermission, router.asPath, router.isReady]);
 
-  if (!router.isReady || isAnonSavedJobs) return null;
+  if (!router.isReady || isAnonSavedJobs || !hasSavedJobs) return null;
 
   return (
     <SidebarSection
