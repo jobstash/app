71c95f703bd3c9300cf4c23dc040102378a6b264
johnshift <johnshift.dev@gmail.com>
2025-01-06 20:22:11 +0800

    refactor: add filters to sentry reports, ignore extensions and errors w/o stack

--- apps/web/sentry.server.config.js
+++ apps/web/sentry.server.config.js
@@ -7,5 +7,17 @@ if (
   Sentry.init({
     dsn: 'https://714cca1095c042fda9f542dde5ed7063@o4504495959703552.ingest.sentry.io/4504519276363776',
     tracesSampleRate: 1,
+    denyUrls: [/extensions\//i, /^chrome:\/\//i, /^chrome-extension:\/\//i],
+    beforeSend(event) {
+      const hasStackTrace = event?.exception?.values?.some(
+        (exceptionValue) => exceptionValue.stacktrace,
+      );
+
+      if (!hasStackTrace) {
+        return null;
+      }
+
+      return event;
+    },
   });
 }
