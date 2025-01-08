f1866dbc7c05fe53f6d15e00782fcc8fa8e850a2
johnshift <johnshift.dev@gmail.com>
2025-01-08 17:06:07 +0800

    refactor: update slugify fn

--- libs/shared/utils/src/lib/normalize-string.ts
+++ libs/shared/utils/src/lib/normalize-string.ts
@@ -1,4 +1,17 @@
-import slugify from 'slugify';
+import slugify from 'slugify'
+import {transliterate} from 'transliteration'
 
-export const normalizeString = (original: string) =>
-  slugify(original, { lower: true });
+export const normalizeString = (str: string | null | undefined): string => {
+  if (str === null || str === undefined) return "";
+  const transliterated = transliterate(str);
+  const slug = slugify(transliterated, {
+    lower: true,
+    remove: /[!"'()*+.:@~]/g,
+    strict: true,
+  });
+  if (slug === "") {
+    return str.trim().toLowerCase();
+  }
+
+  return slug;
+};
