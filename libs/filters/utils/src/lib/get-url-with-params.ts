5eff06d9c56bd74c8f5e23f271a861f54d6ea9aa
johnshift <johnshift.dev@gmail.com>
2025-01-14 22:07:13 +0800

    refactor: exclude privy params from being sent

--- libs/filters/utils/src/lib/get-url-with-params.ts
+++ libs/filters/utils/src/lib/get-url-with-params.ts
@@ -7,6 +7,12 @@ export const getUrlWithParams = (
   const url = new URL(`${origin}${path}`);
   if (Object.keys(params).length > 0) {
     for (const [key, value] of Object.entries(params)) {
+      if (key.startsWith('privy_')) {
+        continue;
+      }
+
+      console.log({ key });
+
       url.searchParams.set(key, value);
     }
   }
